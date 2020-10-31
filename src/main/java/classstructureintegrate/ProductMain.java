d59041e6892b05de65473874913786e8dc988fa0
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 17:34:45 +0100

    Kesz

---
+++ src/main/java/classstructureintegrate/ProductMain.java
@@ -0,0 +1,33 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("A termék neve?");
+        String productName = scanner.nextLine();
+
+        System.out.println("A termék ára?");
+        int productPrice = scanner.nextInt();
+
+        Product product = new Product(productName, productPrice);
+        System.out.println();
+        System.out.println(product.getName());
+        System.out.println(product.getPrice());
+
+        product.increasePrice(23);
+        System.out.println(product.getPrice());
+
+        product.decreasePrice(productPrice);
+        System.out.println(product.getPrice());
+
+
+
+
+
+    }
+}
---
+++ src/main/java/training/TrainerMain.java
@@ -0,0 +1,21 @@
+package training;
+
+import java.util.Scanner;
+
+public class TrainerMain {
+
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Year of birth?");
+        int yearOfBirth = scanner.nextInt();
+
+        Trainer trainer = new Trainer(name, yearOfBirth);
+
+        System.out.println(trainer.getName());
+        System.out.println(trainer.getNameAndYearOfBirth());
+        System.out.println("Age: " + trainer.getAge(2020));
+    }
+}
