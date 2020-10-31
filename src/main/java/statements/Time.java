e845b957c80ce3bddd6405b34d6da916754ad944
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 20:22:42 +0100

    Kesz

---
+++ src/main/java/statements/Time.java
@@ -0,0 +1,31 @@
+package statements;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getInMinutes() {
+        return (hours * 60) + minutes;
+    }
+
+    public int getInSeconds() {
+        return (hours * 3600) + (minutes * 60) + seconds;
+    }
+
+    public boolean earlierThan(Time anotherTime) {
+        return this.getInSeconds() < anotherTime.getInSeconds();
+    }
+
+    public String toString() {
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+
+}
