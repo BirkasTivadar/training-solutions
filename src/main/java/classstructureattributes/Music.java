2128d07bc3713806b9102733ea3dce58db089d7f
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 07:34:43 +0100

    Kesz

---
+++ src/main/java/classstructureattributes/Music.java
@@ -0,0 +1,30 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class Music {
+
+    public static void main(String[] args) {
+
+        Song favoriteSong = new Song();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kedvenc zeneszámának szerzője? ");
+        favoriteSong.band = scanner.nextLine();
+        System.out.println();
+
+        System.out.println("Kedvenc zeneszámának címe? ");
+        favoriteSong.title = scanner.nextLine();
+        System.out.println();
+
+        System.out.println("Kedvenc zeneszámának hossza? ");
+        favoriteSong.length = scanner.nextInt();
+        System.out.println();
+
+        System.out.println("Az ön kedvenc zeneszámának adatai:");
+        System.out.println();
+        System.out.println(favoriteSong.band + " - " + favoriteSong.title + " (" + favoriteSong.length + " perc)" );
+
+    }
+}
