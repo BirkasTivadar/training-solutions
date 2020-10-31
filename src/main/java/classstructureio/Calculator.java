dfc822bb29fb98ead74689038c6e0be3af0e7738
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 07:19:20 +0100

    Kesz

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -1,8 +1,27 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wolrd!");
+
+        System.out.println("Calculator\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérem az első számot: ");
+        int number1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+        System.out.println("Kérem a második számot: ");
+        int number2 = scanner.nextInt();
+
+        int osszeg = number1 + number2;
+
+        System.out.println();
+        System.out.println("A " + number1 + " és a " + number2 + " összege:");
+        System.out.println(number1 + " + " + number2 + " = " + osszeg);
     }
 }
