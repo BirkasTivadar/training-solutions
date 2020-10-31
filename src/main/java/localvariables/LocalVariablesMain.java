08e21e0ba6533f88733d5d412e4df5cc3ad52f11
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 18:58:55 +0100

    Kesz

--- src/main/java/localvariables/LocalVariablesMain.java
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -5,6 +5,12 @@ public class LocalVariablesMain {
     public static void main(String[] args) {
         boolean b = false;
 
+        int a = 2;
+        int i = 3, j = 4;
+        int k = i;
+
+        String s = "Hello World";
+
 
     }
 }
