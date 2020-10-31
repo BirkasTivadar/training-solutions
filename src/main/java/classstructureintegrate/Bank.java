2742780dc00e0e09e994862cacf3cbd7cec9721c
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 18:27:05 +0100

    Kesz

--- src/main/java/classstructureintegrate/Bank.java
+++ src/main/java/classstructureintegrate/Bank.java
@@ -3,20 +3,20 @@ package classstructureintegrate;
 public class Bank {
 
     public static void main(String[] args) {
-        BankAccount egyeniszamla = new BankAccount("1234","Tiva", 30000);
-        BankAccount vallalatiszamla = new BankAccount("5678", "Energo", 1000000);
+        BankAccount egyeniszamla = new BankAccount("1234","Tiva", 30_000);
+        BankAccount vallalatiszamla = new BankAccount("5678", "Energo", 1_000_000);
 
         System.out.println(egyeniszamla.getInfo());
         System.out.println(vallalatiszamla.getInfo());
         System.out.println();
 
-        egyeniszamla.deposit(70000);
-        vallalatiszamla.withdraw(100000);
+        egyeniszamla.deposit(70_000);
+        vallalatiszamla.withdraw(100_000);
         System.out.println(egyeniszamla.getInfo());
         System.out.println(vallalatiszamla.getInfo());
         System.out.println();
 
-        vallalatiszamla.transfer(egyeniszamla, 150000);
+        vallalatiszamla.transfer(egyeniszamla, 150_000);
         System.out.println(egyeniszamla.getInfo());
         System.out.println(vallalatiszamla.getInfo());
 
