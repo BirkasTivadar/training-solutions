5c9c1900751a9f049acc3aa8eb3512bc5868eab8
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 19:35:30 +0100

    Kesz

---
+++ src/main/java/statements/StateMain.java
@@ -0,0 +1,17 @@
+package statements;
+
+public class StateMain {
+
+    public static void main(String[] args) {
+
+        int x = 5 + 6;
+        int y = 11 - x;
+        int z = 8;
+
+        boolean b = (x > y);
+
+        boolean c = b || (z > 5);
+
+        z++;
+    }
+}
