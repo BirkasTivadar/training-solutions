c22937fc74e1b742c0a5a45072919d2c182c548a
BirkasTivadar <birkastivadar@gmail.com>
2020-10-31 07:26:58 +0100

    Kesz

---
+++ src/main/java/classstructuremethods/ClientMain.java
@@ -0,0 +1,23 @@
+package classstructuremethods;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+
+        client.setName("Luis");
+        client.setYear((1980));
+        client.setAddress("Győr");
+
+        System.out.println(client.getName());
+        System.out.println(client.getYear());
+        System.out.println(client.getAddress());
+
+        client.migrate("Sopron");
+
+        System.out.println(client.getAddress());
+
+
+    }
+}
