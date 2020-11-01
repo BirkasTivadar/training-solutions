df201e4a8bb22e98b0d97064964a7e62a5071226
BirkasTivadar <birkastivadar@gmail.com>
2020-11-01 22:57:15 +0100

    Nem kesz.

---
+++ src/main/java/introcontrol/BoatRental.java
@@ -0,0 +1,18 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class BoatRental {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérem a létszámot");
+
+        int letszam = scanner.nextInt();
+
+        if(letszam > 10)
+
+    }
+}
