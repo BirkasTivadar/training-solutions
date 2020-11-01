2aad24a1cf225336224cebcc11cd33049ae72b41
BirkasTivadar <birkastivadar@gmail.com>
2020-11-01 22:44:15 +0100

    Kesz

---
+++ src/main/java/introcontrol/Sum.java
@@ -0,0 +1,22 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+        System.out.println("Kérem az " + i +". számot");
+        int num = scanner.nextInt();
+        sum += num;
+        }
+
+        System.out.println("Az öt szám összege: " + sum);
+
+    }
+
+
+}
