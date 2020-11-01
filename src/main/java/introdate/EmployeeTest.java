db6fe35c6ce4e4c58d35586779de02a55d8e6244
BirkasTivadar <birkastivadar@gmail.com>
2020-11-01 16:13:50 +0100

    Kesz

---
+++ src/main/java/introdate/EmployeeTest.java
@@ -0,0 +1,30 @@
+package introdate;
+
+import java.util.Scanner;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Az új kolléga neve?");
+        String newEmployeeName = scanner.nextLine();
+
+        System.out.println("Az új kolléga születési éve?");
+        int newEmployeeYear = scanner.nextInt();
+
+        System.out.println("Az új kolléga születési hónapja?");
+        int newEmployeeMonth = scanner.nextInt();
+
+        System.out.println("Az új kolléga születési napja?");
+        int newEmployeeDay = scanner.nextInt();
+
+        Employee employee = new Employee(newEmployeeYear, newEmployeeMonth, newEmployeeDay, newEmployeeName);
+
+        System.out.println("Az új kolléga neve: " + employee.getName());
+        System.out.println("Az új kolléga születési dátuma: " + employee.getDateOfBirth());
+        System.out.println("Az új kolléga munkába állásának időpontja: " + employee.getBeginEmployment());
+
+    }
+}
