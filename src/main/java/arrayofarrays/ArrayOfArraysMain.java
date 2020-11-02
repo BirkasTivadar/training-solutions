ee554799d7659fcad6a7305d794ec10139bd03a9
BirkasTivadar <birkastivadar@gmail.com>
2020-11-02 21:16:35 +0100

    Kesz.

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -2,40 +2,46 @@ package arrayofarrays;
 
 public class ArrayOfArraysMain {
 
-    public void tombElemekKiirasa(int[][] tomb){
-        for(int[] sor : tomb){
-            for(int szam : sor){
-                System.out.print(szam+" ");
+    public void tombElemekKiirasa(int[][] tomb) {
+        for (int[] sor : tomb) {
+            for (int szam : sor) {
+                if (szam < 10) {
+                    System.out.print("  ");
+                } else if (szam < 100){
+                    System.out.print(" ");
+                }
+                System.out.print(szam);
+                System.out.print(" ");
             }
             System.out.println();
         }
     }
 
-    public  int[][] multiplicationTable(int size) {
-        int [][] szorzoTabla = new int[size][size];
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                szorzoTabla[i][j] = (i+1)*(j+1);
+    public int[][] multiplicationTable(int size) {
+        int[][] szorzoTabla = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                szorzoTabla[i][j] = (i + 1) * (j + 1);
             }
         }
         return szorzoTabla;
     }
 
-    public int[][] triangularMatrix(int size){
+    public int[][] triangularMatrix(int size) {
         int[][] haromSzogmatrix = new int[size][];
-        for(int i = 0; i < size; i++){
-            haromSzogmatrix[i] = new int[i+1];
-            for(int j = 0; j < haromSzogmatrix[i].length; j++){
+        for (int i = 0; i < size; i++) {
+            haromSzogmatrix[i] = new int[i + 1];
+            for (int j = 0; j < haromSzogmatrix[i].length; j++) {
                 haromSzogmatrix[i][j] = i;
             }
         }
         return haromSzogmatrix;
     }
 
-    public int[][] getValues(){
+    public int[][] getValues() {
         int[] napokSzamaTomb = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int[][] tomb = new int[12][];
-        for(int i = 0; i < 12 ; i++){
+        for (int i = 0; i < 12; i++) {
             tomb[i] = new int[napokSzamaTomb[i]];
         }
         return tomb;
@@ -44,7 +50,7 @@ public class ArrayOfArraysMain {
     public static void main(String[] args) {
         ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
 
-        int[][] arrayOfArray = arrayOfArraysMain.multiplicationTable(3);
+        int[][] arrayOfArray = arrayOfArraysMain.multiplicationTable(23);
         arrayOfArraysMain.tombElemekKiirasa(arrayOfArray);
         System.out.println();
 
@@ -54,7 +60,6 @@ public class ArrayOfArraysMain {
 
         arrayOfArray = arrayOfArraysMain.getValues();
         arrayOfArraysMain.tombElemekKiirasa(arrayOfArray);
-
 
     }
 }
