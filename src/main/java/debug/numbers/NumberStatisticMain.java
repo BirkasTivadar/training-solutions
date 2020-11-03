d1cd77203ded022dc7817836bc0b781e2b351286
BirkasTivadar <birkastivadar@gmail.com>
2020-11-03 21:53:27 +0100

    Kesz.

---
+++ src/main/java/debug/numbers/NumberStatisticMain.java
@@ -0,0 +1,24 @@
+package debug.numbers;
+
+import java.util.Arrays;
+
+public class NumberStatisticMain {
+
+    public static void main(String[] args) {
+
+        NumberStatistics feladat1 = new NumberStatistics(Arrays.asList(4, 8, -1, -2, 4, 5, 3));
+        System.out.println(feladat1.sumPositives());
+        System.out.println(feladat1.minDifferenceBetweenNeighbours());
+
+        NumberStatistics feladat2 = new NumberStatistics(Arrays.asList(-3, -4));
+        System.out.println(feladat2.sumPositives());
+
+        NumberStatistics feladat3 = new NumberStatistics(Arrays.asList(1));
+        System.out.println(feladat3.minDifferenceBetweenNeighbours());
+
+
+
+    }
+
+
+}
