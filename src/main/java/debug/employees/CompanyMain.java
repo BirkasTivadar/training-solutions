3d88320e0ec62dd75af5b3fc297a7d06936028c3
BirkasTivadar <birkastivadar@gmail.com>
2020-11-03 21:29:36 +0100

    Kesz.

---
+++ src/main/java/debug/employees/CompanyMain.java
@@ -0,0 +1,24 @@
+package debug.employees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyMain {
+
+    public static void main(String[] args) {
+        Employee worker = new Employee("Lajos", 1980);
+        Employee accountant = new Employee("Piroska", 1999);
+        Employee secretary = new Employee("Gizike", 2001);
+        Employee director = new Employee("Ferenc", 1973);
+
+
+        Company company = new Company(new ArrayList<>());
+        company.addEmployee(worker);
+        company.addEmployee(accountant);
+        company.addEmployee(secretary);
+        company.addEmployee(director);
+
+        System.out.println(company.listEmployedNames());
+        System.out.println(company.findEmployeeByName("Gizike").getYearOfBirth());
+    }
+}
