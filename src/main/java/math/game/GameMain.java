80bbcc30b89770c93fc5080545bb5c9bc55daf40
BirkasTivadar <birkastivadar@gmail.com>
2020-11-04 19:06:59 +0100

    Kesz.

---
+++ src/main/java/math/game/GameMain.java
@@ -0,0 +1,12 @@
+package math.game;
+
+public class GameMain {
+
+    public static void main(String[] args) {
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(3,4);
+
+        System.out.println(point1.distance(point2));
+    }
+
+}
