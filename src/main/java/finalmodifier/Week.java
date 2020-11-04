85988089f1f7440c2a896879cbabdace77ee41cb
BirkasTivadar <birkastivadar@gmail.com>
2020-11-04 05:42:08 +0100

    Kesz.

---
+++ src/main/java/finalmodifier/Week.java
@@ -0,0 +1,20 @@
+package finalmodifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Week {
+
+    public static final List<String> DAYS_OF_THE_WEEK = Arrays.asList("Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap");
+
+
+    public static void main(String[] args) {
+        DAYS_OF_THE_WEEK.set(2, "Szerda");
+        System.out.println(DAYS_OF_THE_WEEK);
+
+        List<String> dias = Arrays.asList("Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo");
+
+        DAYS_OF_THE_WEEK = dias;
+        System.out.println(DAYS_OF_THE_WEEK);
+    }
+}
