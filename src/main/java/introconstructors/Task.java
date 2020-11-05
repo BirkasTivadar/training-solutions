d180530000e910e9afef2bc4bd9f198808d31b74
BirkasTivadar <birkastivadar@gmail.com>
2020-11-05 23:14:30 +0100

    Kesz.

---
+++ src/main/java/introconstructors/Task.java
@@ -0,0 +1,10 @@
+package introconstructors;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private String title;
+    private String description;
+    private LocalDateTime startDateTime;
+    private int duration;
+}
