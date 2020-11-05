dacd59f4ee1cd61c33c554947307917124403d45
BirkasTivadar <birkastivadar@gmail.com>
2020-11-05 15:43:37 +0100

    Kesz.

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,23 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] nevek = new String[5];
+        String nev;
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Kérem a(z) " + i + ("-dik nevet"));
+            nev = scanner.nextLine();
+            nevek[i - 1] = nev;
+        }
+        for (int i = 1; i < 6; i++) {
+            System.out.println(i + "-dik elem: " + nevek[i - 1]);
+        }
+
+    }
+}
