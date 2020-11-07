1a29466fc381de6cf144328fecf2156f6ce6b09d
BirkasTivadar <birkastivadar@gmail.com>
2020-11-07 11:13:11 +0100

    Kesz.

--- src/main/java/week02/Controller.java
+++ src/main/java/week02/Controller.java
@@ -5,6 +5,7 @@ public class Controller {
     public static void main(String[] args) {
         MeetingRoom targyalo = new MeetingRoom("Tárgyaló", 10, 7);
         MeetingRoom kistargyalo = new MeetingRoom("KisTárgyaló", 4, 5);
+        MeetingRoom fonokitargyalo = new MeetingRoom("Főnöki tárgyaló", 2, 8);
 
         System.out.println(targyalo.getArea());
         System.out.println(kistargyalo.getArea());
@@ -13,8 +14,11 @@ public class Controller {
         Office meetingRooms = new Office();
 
         meetingRooms.addMeetingRoom(targyalo);
+        meetingRooms.addMeetingRoom(fonokitargyalo);
         meetingRooms.addMeetingRoom(kistargyalo);
 
         meetingRooms.printNames();
+        System.out.println();
+        meetingRooms.printNamesReverse();
     }
 }
--- src/main/java/week02/Office.java
+++ src/main/java/week02/Office.java
@@ -1,18 +1,29 @@
 package week02;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Office {
     private List<MeetingRoom> meetingRooms = new ArrayList<>();
 
-    public void addMeetingRoom(MeetingRoom meetingRoom){
+    public void addMeetingRoom(MeetingRoom meetingRoom) {
         meetingRooms.add(meetingRoom);
     }
 
-    public void printNames(){
-        for(MeetingRoom meetingRoom : meetingRooms){
+    public void printNames() {
+        for (MeetingRoom meetingRoom : meetingRooms) {
             System.out.println(meetingRoom.getName());
         }
     }
+
+    public void printNamesReverse(){
+
+        for(int i = meetingRooms.size() - 1; i > -1; i--){
+            MeetingRoom meetingRoom = meetingRooms.get(i);
+            System.out.println(meetingRoom.getName());
+        }
+    }
+
 }
