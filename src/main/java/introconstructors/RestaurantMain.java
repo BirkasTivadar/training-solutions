24d1463fa49ba227d7f70b85595db19b067c3482
BirkasTivadar <birkastivadar@gmail.com>
2020-11-08 17:36:15 +0100

    Kesz.

--- src/main/java/introconstructors/RestaurantMain.java
+++ src/main/java/introconstructors/RestaurantMain.java
@@ -5,7 +5,8 @@ public class RestaurantMain {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant("Két Fenyő", 13);
 
-        System.out.println("Az étterem neve: " + restaurant.getName() + ", kapacitása: " + restaurant.getCapacity() + ", és menü: " +restaurant.getMenu());
+        System.out.println("Az étterem neve: " + restaurant.getName() + ", kapacitása: " + restaurant.getCapacity() + ", és a menü: \n"
+                + restaurant.getMenu().get(0) + "\n" + restaurant.getMenu().get(1) + "\n"+ restaurant.getMenu().get(2));
     }
 
 }
