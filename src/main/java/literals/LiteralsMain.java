3adf66e2dd4c26e39cfb0ad7f58098a4d3c77c90
BirkasTivadar <birkastivadar@gmail.com>
2020-11-09 21:08:22 +0100

    Kesz.

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,29 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+
+        String i = 2 + "" + 3;
+
+        System.out.println(i);
+
+        String j = Integer.toString(2) + Integer.toString(3);
+        System.out.println(j);
+
+        double quotient = 3.0 / 4;
+        System.out.println(quotient);
+
+        long big = 3_244_444_444L;
+
+        String s = "árvíztűrőtükörfúrógép";
+
+        String word = "title".toUpperCase();
+        System.out.println(word);
+
+        System.out.println(Integer.toBinaryString(-3));
+        System.out.println(Integer.toBinaryString(-2));
+
+    }
+
+}
---
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -0,0 +1,12 @@
+package primitivetypes;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+        double x = 5.6;
+        int i = (int) x;
+
+        System.out.println(i);
+    }
+
+}
