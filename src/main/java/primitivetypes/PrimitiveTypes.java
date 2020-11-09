7e226b06fe5cb0e84d0ac89c0437b91e0208dbe5
BirkasTivadar <birkastivadar@gmail.com>
2020-11-09 21:41:50 +0100

    Kesz.

--- src/main/java/primitivetypes/PrimitiveTypes.java
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -2,6 +2,8 @@ package primitivetypes;
 
 public class PrimitiveTypes {
 
+    private final int NUMBER_OF_DIGITS = 32;
+
 
     public String toBinaryString(int n){
         String stringBinary = "";
@@ -10,7 +12,13 @@ public class PrimitiveTypes {
             stringBinary = digit + "" + stringBinary;
             number /= 2;
         }
+        int missingDigits = NUMBER_OF_DIGITS - stringBinary.length();
+        for(int i = 0; i < missingDigits; i++){
+            stringBinary = 0 + stringBinary;
+        }
         return stringBinary;
     }
+
+
 
 }
