c146c3891a4a4808a3beff88294d5ea5b036710f
BirkasTivadar <birkastivadar@gmail.com>
2020-11-09 21:28:29 +0100

    Kesz.

--- src/main/java/primitivetypes/PrimitiveTypes.java
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -2,11 +2,15 @@ package primitivetypes;
 
 public class PrimitiveTypes {
 
-    public static void main(String[] args) {
-        double x = 5.6;
-        int i = (int) x;
 
-        System.out.println(i);
+    public String toBinaryString(int n){
+        String stringBinary = "";
+        for(int number = n; number > 0) {
+            int digit = number % 2;
+            stringBinary += digit;
+            number /= 2;
+        }
+
     }
 
 }
