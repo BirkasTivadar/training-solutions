b5bc2553f555615a13450a3c51fc259a02b039f0
BirkasTivadar <birkastivadar@gmail.com>
2020-11-14 14:03:29 +0100

    Kesz.

--- src/main/java/introjunit/Trainer.java
+++ src/main/java/introjunit/Trainer.java
@@ -1,22 +1,10 @@
 package introjunit;
 
 public class Trainer {
-    private String name;
 
-    public Trainer(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getNameUppercase() {
-        return name.toUpperCase();
-    }
 
     public static void main(String[] args) {
-        String s = "apple pear plum".replace(" p", " g");
-        System.out.println(s);
+
+        System.out.println(new Integer(1) + new Integer(2));
     }
 }
--- src/test/java/introjunit/TrainerTest.java
+++ src/test/java/introjunit/TrainerTest.java
@@ -6,20 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TrainerTest {
 
-    @Test
-    void testCreate() {
-        //Given
-        Trainer trainer = new Trainer("John Doe");
 
-        //When
-        String name = trainer.getName();
 
-        //Then
-        assertEquals("John Doe", name);
-    }
-
-    @Test
-    void testUppercase() {
-        assertEquals("JOHN DOE", new Trainer("john doe").getNameUppercase());
-    }
 }
