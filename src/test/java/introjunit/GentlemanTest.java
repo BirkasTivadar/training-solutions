6aeb48f4d31256f942d8ffe1db0181677afbe619
BirkasTivadar <birkastivadar@gmail.com>
2020-11-14 19:59:07 +0100

    Kesz.

--- src/test/java/introjunit/GentlemanTest.java
+++ src/test/java/introjunit/GentlemanTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GentlemanTest {
 
     @Test
-    void sayHelloTest(){
+    void sayHelloTest() {
         Gentleman gentleman = new Gentleman();
 
         assertEquals("Hello Bibi", gentleman.sayHello("Bibi"));
