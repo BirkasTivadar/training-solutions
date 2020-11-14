c39cc66f2cfa6b975ab1ebfc6742ebe991c180b8
BirkasTivadar <birkastivadar@gmail.com>
2020-11-14 21:33:12 +0100

    Kesz.

---
+++ src/test/java/controlselection/week/DayOfWeeksTest.java
@@ -0,0 +1,17 @@
+package controlselection.week;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayOfWeeksTest {
+
+    @Test
+    void weekMessage() {
+        DayOfWeeks dayOfWeeks = new DayOfWeeks();
+
+        assertEquals("hét eleje", dayOfWeeks.weekMessage("hétfő"));
+        assertEquals("hét közepe", dayOfWeeks.weekMessage("szerda"));
+        assertEquals("hét vége", dayOfWeeks.weekMessage("szombat"));
+    }
+}
