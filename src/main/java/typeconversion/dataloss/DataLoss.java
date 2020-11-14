52a42941b432e7b6a3a4ae85ae01b288041f4624
BirkasTivadar <birkastivadar@gmail.com>
2020-11-14 16:03:22 +0100

    Kesz.

--- src/main/java/typeconversion/dataloss/DataLoss.java
+++ src/main/java/typeconversion/dataloss/DataLoss.java
@@ -4,20 +4,20 @@ import enumtype.week.Day;
 
 public class DataLoss {
 
-        public void dataLoss() {
-            long start = 0;
-            int counter = 0;
-            while (counter < 3) {
-                long variable = (long) (float) start;
-                if (variable != start) {
-                    System.out.println(start);
-                    System.out.println(Math.abs(start-variable));
-                    System.out.println((Long.toBinaryString(start)).length());
-                    counter++;
-                }
-                start++;
+    public void dataLoss() {
+        long start = 0;
+        int counter = 0;
+        while (counter < 3) {
+            long variable = (long) (float) start;
+            if (variable != start) {
+                System.out.println(start);
+                System.out.println(Math.abs(start - variable));
+                System.out.println((Long.toBinaryString(start)).length());
+                counter++;
             }
+            start++;
         }
+    }
 
 
     public static void main(String[] args) {
