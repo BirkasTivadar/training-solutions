5d529da6ed681ed742feed4999ad30f5c64476d7
BirkasTivadar <birkastivadar@gmail.com>
2020-11-14 21:17:14 +0100

    Kesz.

---
+++ src/main/java/controlselection/consonant/ToConsonant.java
@@ -0,0 +1,15 @@
+package controlselection.consonant;
+
+public class ToConsonant {
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static final String VOWELS = "aeiou";
+
+    public char convertToConsonant(char c) {
+        if (VOWELS.indexOf(c) >= 0) {
+           return ALPHABET.charAt(ALPHABET.indexOf(c)+1);
+        } else {
+            return c;
+        }
+    }
+}
