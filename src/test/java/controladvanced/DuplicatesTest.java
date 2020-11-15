200a670f211140704dffb8f2e9785f2ef67e272f
BirkasTivadar <birkastivadar@gmail.com>
2020-11-15 11:59:37 +0100

    Kesz.

--- src/test/java/controladvanced/DuplicatesTest.java
+++ src/test/java/controladvanced/DuplicatesTest.java
@@ -2,11 +2,20 @@ package controladvanced;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DuplicatesTest {
 
     @Test
     void findDuplicates() {
+        assertEquals(Arrays.asList(), new Duplicates().findDuplicates(Arrays.asList(1, 2, 3)));
+        assertEquals(Arrays.asList(2), new Duplicates().findDuplicates(Arrays.asList(1, 2, 2, 3)));
+        assertEquals(Arrays.asList(2, 3), new Duplicates().findDuplicates(Arrays.asList(1, 2, 2, 3, 3, 4)));
+        assertEquals(Arrays.asList(3), new Duplicates().findDuplicates(Arrays.asList(1, 2, 3, 3, 3)));
+        assertEquals(Arrays.asList(2, 3, 4), new Duplicates().findDuplicates(Arrays.asList(1, 2, 2, 3, 3, 2, 4, 4, 3)));
+
+
     }
 }
