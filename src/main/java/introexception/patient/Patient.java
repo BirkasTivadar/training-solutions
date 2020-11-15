758cf19168a0b002d9e04f662a43c14e8184c29b
BirkasTivadar <birkastivadar@gmail.com>
2020-11-15 18:36:15 +0100

    Kesz.

--- src/main/java/introexception/patient/Patient.java
+++ src/main/java/introexception/patient/Patient.java
@@ -8,7 +8,10 @@ public class Patient {
 
     public Patient(String name, String socialSecurityNumber, int yearOfBirth) {
         if(name == null || name.isBlank()){
-            throw new IllegalArgumentException("Name can't be blank.")
+            throw new IllegalArgumentException("Name can't be blank.");
+        }
+        if(yearOfBirth < 1900){
+            throw new IllegalArgumentException("invalid year of birth: "+ yearOfBirth);
         }
         this.name = name;
         this.socialSecurityNumber = socialSecurityNumber;
