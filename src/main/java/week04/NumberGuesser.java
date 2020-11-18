6bd67746d7d3c885737fed0bb6f56b20ea22e4df
BirkasTivadar <birkastivadar@gmail.com>
2020-11-18 15:53:43 +0100

    Kesz.

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -7,22 +7,25 @@ public class NumberGuesser {
 
     public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(99)+1;
+        int number = random.nextInt(99) + 1;
 
         Scanner scanner = new Scanner(System.in);
         int tip = 0;
+        int round = 1;
 
-        while(tip != number){
+        while (round <= 6) {
             System.out.println("Kérem tippeljen egy számot");
             tip = scanner.nextInt();
-            if(tip < number){
+            if (tip < number) {
                 System.out.println("Nem jó, a szám kisebb, mint amit a gép gondolt.");
-            } else {
+            } else if (tip > number) {
                 System.out.println("Nem jó, a szám nagyobb ,mint amit a gép gondolt");
+            } else {
+                System.out.println("Gratulálok, eltalálta a számot.");
             }
+            round++;
         }
-
-        System.out.println("Gratulálok, eltalálta a számot.");
+        System.out.println("Sajnálom, Ön vesztett.");
 
 
     }
