2eb3e5851ff995065cad249cbee43f49bdde6a2a
BirkasTivadar <birkastivadar@gmail.com>
2020-11-18 21:34:47 +0100

    Kesz.

--- src/main/java/stringbuilder/NameBuilder.java
+++ src/main/java/stringbuilder/NameBuilder.java
@@ -1,4 +1,56 @@
 package stringbuilder;
 
 public class NameBuilder {
+
+    private boolean isBlank(String string) {
+        return string == null || string.isBlank();
+    }
+
+    public String concatNameWesternStyle(String familyName, String middleName, String givenName, Title title) {
+        if (isBlank(familyName) || isBlank(givenName)) {
+            throw new IllegalArgumentException("Family name and given name must not be empty!");
+        }
+
+        StringBuilder name = new StringBuilder();
+
+        if (title != null) {
+            name.append(title.getTitle()).append(" ").append(givenName);
+        } else {
+            name.append(givenName);
+        }
+        if (!isBlank(middleName)) {
+            name.append(" ").append(middleName);
+        }
+        name.append(" ").append(familyName);
+
+        return name.toString();
+    }
+
+    public String concatNameHungarianStyle(String familyName, String middleName, String givenName, Title title) {
+        if (isBlank(familyName) || isBlank(givenName)) {
+            throw new IllegalArgumentException("Family name and given name must not be empty!");
+        }
+
+        StringBuilder name = new StringBuilder();
+
+        if (title != null) {
+            name.append(title.getTitle()).append(" ").append(familyName);
+        } else {
+            name.append(familyName);
+        }
+        if (!isBlank(middleName)) {
+            name.append(" ").append(middleName);
+        }
+        name.append(" ").append(givenName);
+
+        return name.toString();
+    }
+/*
+    public String insertTitle(String name, Title title, String where){
+
+    }
+
+    public String deleteNamePart(String name, String delete){
+
+    }*/
 }
---
+++ src/test/java/stringbuilder/NameBuilderTest.java
@@ -0,0 +1,53 @@
+package stringbuilder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameBuilderTest {
+
+    @Test
+    void invalidParametersShouldThrowExceptionNullFamilyName() {
+        NameBuilder nameBuilder = new NameBuilder();
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> nameBuilder.concatNameHungarianStyle(null, "G", "John", Title.MR));
+        assertEquals("Family name and given name must not be empty!", iae.getMessage());
+    }
+
+    @Test
+    void invalidParametersShouldThrowExceptionNullGivenName() {
+        NameBuilder nameBuilder = new NameBuilder();
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> nameBuilder.concatNameHungarianStyle("Smith", "G", null, Title.MR));
+        assertEquals("Family name and given name must not be empty!", iae.getMessage());
+    }
+
+    @Test
+    void invalidParametersShouldThrowExceptionEmptyFamilyName() {
+        NameBuilder nameBuilder = new NameBuilder();
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> nameBuilder.concatNameHungarianStyle("", "G", "John", Title.MR));
+        assertEquals("Family name and given name must not be empty!", iae.getMessage());
+    }
+ @Test
+    void invalidParametersShouldThrowExceptionEmptyGivenName() {
+        NameBuilder nameBuilder = new NameBuilder();
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> nameBuilder.concatNameHungarianStyle("Smith", "G", "", Title.MR));
+        assertEquals("Family name and given name must not be empty!", iae.getMessage());
+    }
+
+    @Test
+    void westernStyleNameConcatenation() {
+        NameBuilder nameBuilder = new NameBuilder();
+        assertEquals("Mr. John G Smith", nameBuilder.concatNameWesternStyle("Smith", "G", "John", Title.MR));
+        assertEquals("John G Smith", nameBuilder.concatNameWesternStyle("Smith", "G", "John", null));
+        assertEquals("Mr. John Smith", nameBuilder.concatNameWesternStyle("Smith", "", "John", Title.MR));
+        assertEquals("Mr. John Smith", nameBuilder.concatNameWesternStyle("Smith", null, "John", Title.MR));
+    }
+
+    @Test
+    void hungarianStyleNameConcatenation() {
+        NameBuilder nameBuilder = new NameBuilder();
+        assertEquals("Mr. Smith G John", nameBuilder.concatNameHungarianStyle("Smith", "G", "John", Title.MR));
+        assertEquals("Smith G John", nameBuilder.concatNameHungarianStyle("Smith", "G", "John", null));
+        assertEquals("Mr. Smith John", nameBuilder.concatNameHungarianStyle("Smith", "", "John", Title.MR));
+        assertEquals("Mr. Smith John", nameBuilder.concatNameHungarianStyle("Smith", null, "John", Title.MR));
+    }
+}
