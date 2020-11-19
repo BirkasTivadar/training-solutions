0724851f1bc12bdff3fb1c05b43a752897315152
BirkasTivadar <birkastivadar@gmail.com>
2020-11-19 19:39:13 +0100

    Kesz.

---
+++ src/main/java/stringscanner/StringScanner.java
@@ -0,0 +1,20 @@
+package stringscanner;
+
+public class StringScanner {
+
+    public int readAndSumValues(String intString, String delimiter){
+
+    }
+    /*
+    public int readAndSumValues(String intString){
+
+    }
+
+    public String filterLinesWithWordOccurrences(String text, String word){
+
+    }*/
+
+    public boolean isBlank(String string){
+            return string == null || string.isBlank();
+    }
+}
