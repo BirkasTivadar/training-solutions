e83f78ea7eab1dcd41649ee8c80a3d114401c2de
BirkasTivadar <birkastivadar@gmail.com>
2020-11-20 20:08:44 +0100

    Kesz.

---
+++ src/main/java/algorithmssum/SalesAmountSumCalculator.java
@@ -0,0 +1,15 @@
+package algorithmssum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalesAmountSumCalculator {
+
+    public int sumSalesAmount(List<Salesperson> salespersonList) {
+        int sum = 0;
+        for (Salesperson salesperson : salespersonList) {
+            sum += salesperson.getAmount();
+        }
+        return sum;
+    }
+}
