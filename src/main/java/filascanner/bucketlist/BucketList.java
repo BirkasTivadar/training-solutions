07c6cfc07673773f052e82aee6f2dbf26d6d681c
BirkasTivadar <birkastivadar@gmail.com>
2020-11-20 12:15:55 +0100

    Kesz.

--- src/main/java/filascanner/bucketlist/BucketList.java
+++ src/main/java/filascanner/bucketlist/BucketList.java
@@ -1,18 +1,19 @@
-package training;
+package filascanner.bucketlist;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-public class FileScannerMain {
+public class BucketList {
 
     public static void main(String[] args) {
-
-        try (Scanner scanner = new Scanner(FileScannerMain.class.getResourceAsStream("/employees.txt"))) {
+        try (Scanner scanner = new Scanner(Path.of("bucketlist.txt"))) {
             while (scanner.hasNextLine()) {
                 String row = scanner.nextLine();
                 System.out.println(row);
             }
+        } catch (IOException ie) {
+            System.out.println("Cannot read bucketlist.txt file.");
         }
     }
 }
