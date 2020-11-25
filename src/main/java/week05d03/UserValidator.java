24818ca3b32218ebb34dd5fd6182e9d07e3c988a
BirkasTivadar <birkastivadar@gmail.com>
2020-11-25 18:39:08 +0100

    Kesz.

---
+++ src/main/java/week05d03/UserValidator.java
@@ -0,0 +1,22 @@
+package week05d03;
+
+import java.util.List;
+
+public class UserValidator {
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isBlank();
+    }
+
+    public boolean vallidate(List<User> users) {
+        for (User user : users) {
+            if (isEmpty(user.getName())) {
+                throw new IllegalArgumentException("Name must not be empty.");
+            }
+            if (user.getAge() < 0 || user.getAge() > 120) {
+                throw new IllegalArgumentException("Age must be between 0 and 120.");
+            }
+        }
+        return true;
+    }
+}
