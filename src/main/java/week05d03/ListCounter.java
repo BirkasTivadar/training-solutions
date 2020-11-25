822cb61a3109ae3bcbfa291a6b18e495421a8f70
BirkasTivadar <birkastivadar@gmail.com>
2020-11-25 15:49:04 +0100

    Kesz.

--- src/main/java/week05d03/ListCounter.java
+++ src/main/java/week05d03/ListCounter.java
@@ -1,5 +1,6 @@
 package week05d03;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListCounter {
@@ -14,5 +15,15 @@ public class ListCounter {
             }
         }
         return counter;
+    }
+
+    public static void main(String[] args) {
+        ListCounter listCounter = new ListCounter();
+        List<String> list = new ArrayList<>();
+        list.add("asde");
+        list.add("bsde");
+        list.add("zsde");
+        list.add("Asde");
+        System.out.println(listCounter.counterStartA(list));
     }
 }
