0937918fed95a26be438eb640abda34b0c49e1fa
BirkasTivadar <birkastivadar@gmail.com>
2020-11-28 17:12:09 +0100

    Kesz.

--- src/main/java/week05d04/ProductSenior.java
+++ src/main/java/week05d04/ProductSenior.java
@@ -2,31 +2,31 @@ package week05d04;
 
 public class ProductSenior {
     private long price;
-    private String currency;
+    private CurrencyType currency;
 
-    public ProductSenior(long price, String currency) {
-        if (!currency.equals("HUF") && !currency.equals("USD")) {
+    public ProductSenior(long price, CurrencyType currency) {
+        if (currency != CurrencyType.HUF && currency != CurrencyType.USD) {
             throw new IllegalArgumentException("Currency must be HUF or USD");
         }
         this.price = price;
         this.currency = currency;
     }
 
-    public double convertPrice(String currency) {
-        if (this.currency.equals(currency)) {
+    public double convertPrice(CurrencyType currency) {
+        if (this.currency == currency) {
             return price;
         }
-        if (currency.equals("HUF")) {
-            return price * 300.0;
+        if (currency == CurrencyType.HUF) {
+            return price * CurrencyType.USD.getValue();
         }
-        return price / 300.0;
+        return price / CurrencyType.USD.getValue();
     }
 
     public long getPrice() {
         return price;
     }
 
-    public String getCurrency() {
+    public CurrencyType getCurrency() {
         return currency;
     }
 }
--- src/test/java/week05d04/ProductSeniorTest.java
+++ src/test/java/week05d04/ProductSeniorTest.java
@@ -8,21 +8,21 @@ class ProductSeniorTest {
 
     @Test
     void createProductSenior() {
-        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new ProductSenior(1500, "EUR"));
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new ProductSenior(1500, null));
         assertEquals("Currency must be HUF or USD", iae.getMessage());
     }
 
     @Test
     void convertPriceHUF() {
-        ProductSenior productSenior = new ProductSenior(1500, "HUF");
-        assertEquals(5, productSenior.convertPrice("USD"));
-        assertEquals(1500, productSenior.convertPrice("HUF"));
+        ProductSenior productSenior = new ProductSenior(1500, CurrencyType.HUF);
+        assertEquals(5, productSenior.convertPrice(CurrencyType.USD));
+        assertEquals(1500, productSenior.convertPrice(CurrencyType.HUF));
     }
 
     @Test
     void convertPriceUSD() {
-        ProductSenior productSenior = new ProductSenior(22, "USD");
-        assertEquals(22, productSenior.convertPrice("USD"));
-        assertEquals(6600, productSenior.convertPrice("HUF"));
+        ProductSenior productSenior = new ProductSenior(22, CurrencyType.USD);
+        assertEquals(22, productSenior.convertPrice(CurrencyType.USD));
+        assertEquals(6600, productSenior.convertPrice(CurrencyType.HUF));
     }
 }
