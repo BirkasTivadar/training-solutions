d6494ee1fdd1cb3bece298f60b017ad9e4a07e62
BirkasTivadar <birkastivadar@gmail.com>
2020-11-29 11:25:29 +0100

    Kesz.

---
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -0,0 +1,44 @@
+package stringmethods.url;
+
+public class UrlManager {
+    private String protocol;
+    private int port;
+    private String host;
+    private String path;
+    private String query;
+
+    public UrlManager(String url) {
+        this.protocol = protocol;
+        this.port = port;
+        this.host = host;
+        this.path = path;
+        this.query = query;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void validateString(String str){
+        if(str == null || str.isBlank()){
+            throw new IllegalArgumentException("Parameter must be not empty.");
+        }
+    }
+
+    public boolean hasProperty(String key){
+        validateString(key);
+        return query.startsWith((key + "=") || query.contains("&"+ key + "="));
+    }
+}
