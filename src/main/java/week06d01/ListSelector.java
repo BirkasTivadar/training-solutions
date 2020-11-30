62c1da7b9653bb86ba5d7683d2ea4303eb2534a5
BirkasTivadar <birkastivadar@gmail.com>
2020-11-30 15:51:37 +0100

    Kesz.

--- src/main/java/week06d01/ListSelector.java
+++ src/main/java/week06d01/ListSelector.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class ListSelector {
 
 
-    public String concatEvenIndexElements(List<String> lista){
-        if(lista==null){
+    public String concatEvenIndexElements(List<String> lista) {
+        if (lista == null) {
             throw new NullPointerException("List must be not null.");
         }
-        if(lista.size() == 0){
+        if (lista.size() == 0) {
             return "";
         }
         String result = "[";
-        for(int i = 0; i< lista.size(); i +=2){
-            result += lista.get(i)+ ",";
+        for (int i = 0; i < lista.size(); i += 2) {
+            result += lista.get(i) + ",";
         }
-        result = result.substring(0, result.length()-1);
+        result = result.substring(0, result.length() - 1);
         result += "]";
         return result;
     }
--- src/test/java/week06d01/ListSelectorTest.java
+++ src/test/java/week06d01/ListSelectorTest.java
@@ -11,7 +11,13 @@ class ListSelectorTest {
     @Test
     void concatEvenIndexElementsTest() {
         ListSelector listSelector = new ListSelector();
-
-        assertEquals("[alma,körte]", listSelector.concatEvenIndexElements(Arrays.asList("alma","barack","körte")));
+        assertEquals("[alma,körte]", listSelector.concatEvenIndexElements(Arrays.asList("alma", "barack", "körte")));
     }
+
+    @Test
+    void concatNullListTest() {
+        ListSelector listSelector = new ListSelector();
+        assertEquals("[alma,körte]", listSelector.concatEvenIndexElements(Arrays.asList("alma", "barack", "körte")));
+    }
+
 }
