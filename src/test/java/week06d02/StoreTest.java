4540fdc66d45cab630c07bbf9cccd6a2944df3d4
BirkasTivadar <birkastivadar@gmail.com>
2020-12-01 15:53:11 +0100

    Kesz.

--- src/test/java/week06d02/StoreTest.java
+++ src/test/java/week06d02/StoreTest.java
@@ -2,12 +2,21 @@ package week06d02;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StoreTest {
 
     @Test
     void getProductByCategoryTest() {
-        Product butter = new Product("butter", )
+        Product bread = new Product("bread", Category.BAKEDGOODS,120);
+        Product butter = new Product("butter", Category.DAIRY,100);
+        Product baguette = new Product("baguette", Category.BAKEDGOODS,150);
+        Product milk = new Product("milk", Category.DAIRY,10);
+
+        Store store = new Store(Arrays.asList(bread, butter, baguette, milk));
+
+        assertEquals(2, store.getProductByCategory(Category.BAKEDGOODS));
     }
 }
