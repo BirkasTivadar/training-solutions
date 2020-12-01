b5909f36977e0b7572a6c26710b043cd741476b2
BirkasTivadar <birkastivadar@gmail.com>
2020-12-01 20:04:59 +0100

    Kesz.

--- src/main/java/week06d02/Store.java
+++ src/main/java/week06d02/Store.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
-    List<Product> products = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
 
     public Store(List<Product> products) {
         this.products = products;
@@ -12,8 +16,8 @@ public class Store {
 
     public int getProductByCategory(Category category) {
         int db = 0;
-        for (Product product : products){
-            if(product.getCategory() == category){
+        for (Product product : products) {
+            if (product.getCategory() == category) {
                 db++;
             }
         }
--- src/test/java/week06d02/StoreTest.java
+++ src/test/java/week06d02/StoreTest.java
@@ -10,13 +10,16 @@ class StoreTest {
 
     @Test
     void getProductByCategoryTest() {
-        Product bread = new Product("bread", Category.BAKEDGOODS,120);
-        Product butter = new Product("butter", Category.DAIRY,100);
-        Product baguette = new Product("baguette", Category.BAKEDGOODS,150);
-        Product milk = new Product("milk", Category.DAIRY,10);
+        Product bread = new Product("bread", Category.BAKEDGOODS, 120);
+        Product butter = new Product("butter", Category.DAIRY, 100);
+        Product baguette = new Product("baguette", Category.BAKEDGOODS, 150);
+        Product milk = new Product("milk", Category.DAIRY, 10);
+        Product sugar = new Product("sugar", Category.OTHER, 20);
 
-        Store store = new Store(Arrays.asList(bread, butter, baguette, milk));
+        Store store = new Store(Arrays.asList(bread, butter, baguette, milk, sugar));
 
         assertEquals(2, store.getProductByCategory(Category.BAKEDGOODS));
+        assertEquals(2, store.getProductByCategory(Category.DAIRY));
+        assertEquals(1, store.getProductByCategory(Category.OTHER));
     }
 }
