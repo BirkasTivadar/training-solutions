1bda3d8777a1ffd616fb63aa46ba7ff0b38ab65e
BirkasTivadar <birkastivadar@gmail.com>
2020-12-04 12:18:48 +0100

    Kesz.

--- src/main/java/methodoverloading/pub/ListOfGoodPubs.java
+++ src/main/java/methodoverloading/pub/ListOfGoodPubs.java
@@ -7,16 +7,16 @@ public class ListOfGoodPubs {
     private List<Pub> goodPubs;
 
     public ListOfGoodPubs(List<Pub> goodPubs) {
-        if(goodPubs == null || goodPubs.size() == 0){
+        if (goodPubs == null || goodPubs.size() == 0) {
             throw new IllegalArgumentException("Pub list is empty!");
         }
         this.goodPubs = new ArrayList<>(goodPubs);
     }
 
-    public Pub findTheBest(){
+    public Pub findTheBest() {
         Pub bestpub = goodPubs.get(0);
-        for(Pub pub : goodPubs){
-            if(pub.getOpenFrom().isEarlier(bestpub.getOpenFrom())){
+        for (Pub pub : goodPubs) {
+            if (pub.getOpenFrom().isEarlier(bestpub.getOpenFrom())) {
                 bestpub = pub;
             }
         }
