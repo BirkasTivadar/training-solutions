a74c504c9fcd6a6af738c92d5125f49d25a730dc
BirkasTivadar <birkastivadar@gmail.com>
2020-12-06 08:44:43 +0100

    Kesz.

---
+++ src/main/java/methodpass/troopers/Trooper.java
@@ -0,0 +1,39 @@
+package methodpass.troopers;
+
+public class Trooper {
+    private String name;
+
+    private Position position;
+
+    public String getName() {
+        return name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public boolean isEmpty(String str){
+        return str == null || str.isBlank();
+    }
+
+    public Trooper(String name) {
+        if(isEmpty(name)){
+            throw new IllegalArgumentException("Name must not be empty.");
+        }
+        this.name = name;
+        this.position = new Position(0,0);
+    }
+
+    public void changePosition(Position target){
+        if(target == null){
+            throw new IllegalArgumentException("The new position must not be null");
+        }
+        this.position = new Position(target.getPosX(), target.getPosY());
+    }
+
+    public double distanceFrom(Position target){
+       return this.position.distanceFrom(target);
+    }
+
+}
