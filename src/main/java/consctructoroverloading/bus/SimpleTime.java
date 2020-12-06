8e06fa247922e182f2db2c3a1372a15b680eac2e
BirkasTivadar <birkastivadar@gmail.com>
2020-12-06 19:15:46 +0100

    Kesz.

---
+++ src/main/java/consctructoroverloading/bus/SimpleTime.java
@@ -0,0 +1,28 @@
+package consctructoroverloading.bus;
+
+public class SimpleTime {
+    private int hours;
+    private int minutes;
+
+    public SimpleTime(int hours) {
+        this.hours = hours;
+    }
+
+    public SimpleTime(int hours, int minutes) {
+        this(hours);
+        this.minutes = minutes;
+    }
+
+    public SimpleTime(SimpleTime time) {
+        this.hours = time.hours;
+        this.minutes = time.minutes;
+    }
+
+    public int difference(SimpleTime time) {
+        return (this.hours * 60 + this.minutes) - (time.hours * 60 + time.minutes);
+    }
+
+    public String toString() {
+        return this.hours + ":" + this.minutes;
+    }
+}
