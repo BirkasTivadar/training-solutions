e738afe551bd9ca69412a37c6835fabb2b544da6
BirkasTivadar <birkastivadar@gmail.com>
2020-12-06 20:01:28 +0100

    Kesz.

---
+++ src/main/java/consctructoroverloading/bus/BusTimeTable.java
@@ -0,0 +1,33 @@
+package consctructoroverloading.bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusTimeTable {
+    private List<SimpleTime> timeTable = new ArrayList<>();
+
+    public List<SimpleTime> getTimeTable() {
+        return timeTable;
+    }
+
+    public BusTimeTable(List<SimpleTime> timeTable) {
+        this.timeTable = timeTable;
+    }
+
+    public BusTimeTable(int firstHour, int lastHour, int everyMinute) {
+        List<SimpleTime> createdTimeTable = new ArrayList<>();
+        for (int i = firstHour; i <= lastHour; i++) {
+            createdTimeTable.add(new SimpleTime(i, everyMinute));
+        }
+        this.timeTable = createdTimeTable;
+    }
+
+    public SimpleTime nextBus(SimpleTime actual) {
+        for (SimpleTime simpleTime : timeTable) {
+            if (simpleTime.difference(actual) >= 0) {
+                return simpleTime;
+            }
+        }
+        throw new IllegalArgumentException("No more buses today!");
+    }
+}
