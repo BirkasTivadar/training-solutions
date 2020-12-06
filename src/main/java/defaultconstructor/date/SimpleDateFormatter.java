22eda2f5d539737836cbb2c992d37bb8161b0fa2
BirkasTivadar <birkastivadar@gmail.com>
2020-12-06 12:07:57 +0100

    Kesz.

--- src/main/java/defaultconstructor/date/SimpleDateFormatter.java
+++ src/main/java/defaultconstructor/date/SimpleDateFormatter.java
@@ -2,17 +2,14 @@ package defaultconstructor.date;
 
 public class SimpleDateFormatter {
     private CountryCode code;
-    private SimpleDate simpleDate;
 
     public SimpleDateFormatter() {
         this.code = CountryCode.HU;
-        this.simpleDate = simpleDate;
     }
 
     public String formatDateString(SimpleDate simpleDate) {
         return simpleDate.getYear() + "-" + simpleDate.getMonth() + "-" + simpleDate.getDay();
     }
-
 
     public String formatDateStringByCountryCode(CountryCode countryCode, SimpleDate simpleDate) {
         switch (countryCode) {
