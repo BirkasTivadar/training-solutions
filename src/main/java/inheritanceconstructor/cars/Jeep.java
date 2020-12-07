487bcdc438821ddb1fc23248f44c31398f181702
BirkasTivadar <birkastivadar@gmail.com>
2020-12-07 15:08:03 +0100

    Kesz.

---
+++ src/main/java/inheritanceconstructor/cars/Jeep.java
@@ -0,0 +1,17 @@
+package inheritanceconstructor.cars;
+
+public class Jeep extends Car {
+    private double extraCapacity;
+    private double extraFuel;
+
+    public Jeep(double fuelRate, double fuel, double tankCapacity, double extraCapacity, double extraFuel) {
+        super(fuelRate, fuel, tankCapacity);
+        if (extraFuel > extraCapacity) {
+            throw new IllegalArgumentException("Extra capacity is less than extra fuel!");
+        }
+        this.extraCapacity = extraCapacity;
+        this.extraFuel = extraFuel;
+    }
+
+
+}
