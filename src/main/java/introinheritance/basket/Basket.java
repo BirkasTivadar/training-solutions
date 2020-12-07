4601da626f4ae05a7b62f84050812145bb1ad1bf
BirkasTivadar <birkastivadar@gmail.com>
2020-12-07 10:44:23 +0100

    Kesz.

---
+++ src/main/java/introinheritance/basket/Basket.java
@@ -0,0 +1,30 @@
+package introinheritance.basket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+    private List<Item> items = new ArrayList<>();
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public void removeItem(String barcode){
+        Item findItem = null;
+        for(Item item : items){
+            if(item.getBarcode().equals(barcode)){
+                findItem = item;
+            }
+        }
+        items.remove(findItem);
+    }
+
+    public void clearBasket(){
+        items.removeAll(items);
+    }
+}
