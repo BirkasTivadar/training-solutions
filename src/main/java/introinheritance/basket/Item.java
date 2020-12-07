2ad16608ce9c7950bcf49203383e268480131503
BirkasTivadar <birkastivadar@gmail.com>
2020-12-07 10:35:54 +0100

    Kesz.

--- src/main/java/introinheritance/basket/Item.java
+++ src/main/java/introinheritance/basket/Item.java
@@ -1,5 +1,29 @@
 package introinheritance.basket;
 
 public class Item {
-    private
+    private String barcode;
+    private double nettoPrice;
+    private int vatPercent;
+
+    public Item(String barcode, double nettoPrice, int vatPercent) {
+        this.barcode = barcode;
+        this.nettoPrice = nettoPrice;
+        this.vatPercent = vatPercent;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public double getNettoPrice() {
+        return nettoPrice;
+    }
+
+    public double getTaxAmount() {
+        return nettoPrice * vatPercent / 100.0;
+    }
+
+    public String toString() {
+        return "Item{barcode='" + barcode + "', nettoPrice=" + nettoPrice + ", vatPercent=" + vatPercent + "}";
+    }
 }
