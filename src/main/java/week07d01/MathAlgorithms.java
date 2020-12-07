9d676fc46a20df8e03dc234cc7f1d3108deeb58a
BirkasTivadar <birkastivadar@gmail.com>
2020-12-07 15:36:17 +0100

    Kesz.

---
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -0,0 +1,18 @@
+package week07d01;
+
+public class MathAlgorithms {
+
+    public static boolean isPrime(int x){
+        if(x<=0){
+            throw new IllegalArgumentException("Number must be greater than zero.");
+        }
+        for(int i = 2; i < x; i++){
+            if(x%i == 0){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+}
