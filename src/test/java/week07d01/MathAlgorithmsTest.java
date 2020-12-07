563106323aa535b63f193d0dc9b74b4b3ac7e1df
BirkasTivadar <birkastivadar@gmail.com>
2020-12-07 15:41:42 +0100

    Kesz.

--- src/test/java/week07d01/MathAlgorithmsTest.java
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -12,18 +12,31 @@ class MathAlgorithmsTest {
         assertTrue(mathAlgorithms.isPrime(2));
     }
 
-     @Test
+    @Test
     void isPrime1() {
         assertFalse(mathAlgorithms.isPrime(1));
     }
-    /*
-     @Test
-    void isPrime() {
-        assertTrue(mathAlgorithms.isPrime(2));
+
+    @Test
+    void isPrime13() {
+        assertTrue(mathAlgorithms.isPrime(13));
     }
-     @Test
-    void isPrime() {
-        assertTrue(mathAlgorithms.isPrime(2));
-    }*/
+
+    @Test
+    void isPrime31() {
+        assertTrue(mathAlgorithms.isPrime(31));
+    }
+    @Test
+    void isPrime10() {
+        assertFalse(mathAlgorithms.isPrime(10));
+    }
+    @Test
+    void isPrime21() {
+        assertFalse(mathAlgorithms.isPrime(21));
+    }
+    @Test
+    void isPrime150() {
+        assertFalse(mathAlgorithms.isPrime(150));
+    }
 
 }
