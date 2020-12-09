92aef5c39c93b5040213443ed46c55abf78e30cd
BirkasTivadar <birkastivadar@gmail.com>
2020-12-09 11:47:06 +0100

    Kesz.

---
+++ src/main/java/inheritancemethods/products/PackedProduct.java
@@ -0,0 +1,28 @@
+package inheritancemethods.products;
+
+
+import java.math.BigDecimal;
+
+public class PackedProduct extends Product {
+    private int packingUnit;
+    private BigDecimal weightOfBox;
+
+    public PackedProduct(String name, BigDecimal unitWeight, int numberOfDecimals, int packingUnit, BigDecimal weightOfBox) {
+        super(name, unitWeight, numberOfDecimals);
+        this.packingUnit = packingUnit;
+        this.weightOfBox = weightOfBox;
+    }
+
+    public int getPackingUnit() {
+        return packingUnit;
+    }
+
+    public BigDecimal getWeightOfBox() {
+        return weightOfBox;
+    }
+
+    @Override
+    public BigDecimal totalWeight(int pieces) {
+        return super.totalWeight(pieces).add(weightOfBox.multiply(new BigDecimal(String.valueOf(packingUnit))));
+    }
+}
