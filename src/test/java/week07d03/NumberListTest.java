0de0e16e61ac1c72038caabdb19a3f14d5d31f84
BirkasTivadar <birkastivadar@gmail.com>
2020-12-09 15:43:46 +0100

    Kesz.

--- src/test/java/week07d03/NumberListTest.java
+++ src/test/java/week07d03/NumberListTest.java
@@ -21,6 +21,8 @@ class NumberListTest {
         assertTrue(numberList.isIncreasing(Arrays.asList(1)));
         assertTrue(numberList.isIncreasing(Arrays.asList(1, 1, 1, 1)));
         assertFalse(numberList.isIncreasing(Arrays.asList(1, 3, 2, 5)));
+        assertFalse(numberList.isIncreasing(Arrays.asList(1, 2,3,4,5,6,5)));
+        assertFalse(numberList.isIncreasing(Arrays.asList(2,1,2,2)));
     }
 
 
