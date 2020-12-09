154f8a3f64ec60376e4d428410396be0452bd297
BirkasTivadar <birkastivadar@gmail.com>
2020-12-09 20:40:10 +0100

    Kesz.

---
+++ src/test/java/abstractclass/gamecharacter/BattleField.java
@@ -0,0 +1,32 @@
+package abstractclass.gamecharacter;
+
+public class BattleField {
+    private int round;
+
+    public BattleField(int round) {
+        this.round = round;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    private boolean oneHit(Character attacker, Character defender) {
+        if (attacker.isAlive() && defender.isAlive()) {
+            attacker.primaryAttack(defender);
+            attacker.secondaryAttack(defender);
+            return true;
+        }
+        return false;
+    }
+
+    public Character fight(Character one, Character other) {
+        while(one.isAlive() && other.isAlive()){
+            round++;
+            oneHit(one,other);
+            oneHit(other,one);
+        }
+        return one.isAlive() ? one : other;
+    }
+
+}
---
+++ src/test/java/abstractclass/gamecharacter/BattleFieldTest.java
@@ -0,0 +1,7 @@
+package abstractclass.gamecharacter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BattleFieldTest {
+
+}
