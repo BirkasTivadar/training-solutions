8dcee0bdd7d65a39445691c3d91a896fba7d80af
BirkasTivadar <birkastivadar@gmail.com>
2020-12-11 10:11:49 +0100

    Kesz.

--- src/main/java/interfaces/simplethread/SimpleThread.java
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -1,4 +1,29 @@
 package interfaces.simplethread;
 
-public class SimpleThread {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleThread implements Runnable{
+    private List<String> task = new ArrayList<>();
+
+    public SimpleThread(List<String> task) {
+        this.task = task;
+    }
+
+    public List<String> getTask() {
+        return task;
+    }
+
+    private boolean nextStep(){
+        task.remove(task.size()-1);
+        return task.isEmpty() ? false : true;
+
+    }
+
+    @Override
+    public void run() {
+        while(nextStep()){
+            nextStep();
+        }
+    }
 }
