81db64d13fe1c73036795c0a4f0cf062b67a960d
BirkasTivadar <birkastivadar@gmail.com>
2020-12-13 12:51:25 +0100

    Kesz.

--- src/main/java/interfacestaticmethods/Valued.java
+++ src/main/java/interfacestaticmethods/Valued.java
@@ -6,11 +6,11 @@ public interface Valued {
 
     double getValued();
 
-    static double totalValue(List<Valued> valueds){
-       double sum = 0;
-       for(Valued valued : valueds){
-           sum += valued.getValued();
-       }
-       return sum;
+    static double totalValue(List<Valued> valueds) {
+        double sum = 0;
+        for (Valued valued : valueds) {
+            sum += valued.getValued();
+        }
+        return sum;
     }
 }
