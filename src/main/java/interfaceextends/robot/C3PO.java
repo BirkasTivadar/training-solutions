bdfaeed787cadf58a4474de9795eb61692da9f31
BirkasTivadar <birkastivadar@gmail.com>
2020-12-13 08:57:42 +0100

    Kesz.

--- src/main/java/interfaceextends/robot/C3PO.java
+++ src/main/java/interfaceextends/robot/C3PO.java
@@ -1,4 +1,50 @@
 package interfaceextends.robot;
 
-public class C3PO {
+import java.util.ArrayList;
+import java.util.List;
+
+public class C3PO implements MoveableRobot {
+    private Point position;
+    private int angle;
+    List<Point> path = new ArrayList<>();
+
+    public C3PO(Point position) {
+        this.position = position;
+    }
+
+    private void walkTo(Point position){
+        this.position = position;
+        path.add(position);
+    }
+
+    @Override
+    public void moveTo(Point position) {
+        walkTo(position);
+
+    }
+
+    @Override
+    public void fastMoveTo(Point position) {
+        walkTo(position);
+
+    }
+
+    @Override
+    public void rotate(int angle) {
+        this.angle = angle;
+
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+
+    @Override
+    public List<Point> getPath() {
+        return path;
+    }
 }
