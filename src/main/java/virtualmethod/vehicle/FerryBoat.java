0d017d3d333c823d066362777026c4d43fd66144
BirkasTivadar <birkastivadar@gmail.com>
2020-12-13 21:33:29 +0100

    Kesz.

--- src/main/java/virtualmethod/vehicle/FerryBoat.java
+++ src/main/java/virtualmethod/vehicle/FerryBoat.java
@@ -1,24 +1,28 @@
 package virtualmethod.vehicle;
 
-public class FerryBoat {
+public class FerryBoat extends Vehicle {
+    private Car car;
+    public static final int MAX_CARRY_WEIGHT = 2000;
 
-    public final int MAX_CARRY_WEIGHT;
-    private Car car = null;
-
-    public FerryBoat(int max) {
-        MAX_CARRY_WEIGHT = max;
+    public FerryBoat(int vehicleWeight) {
+        super(vehicleWeight);
     }
 
     public boolean canCarry(Car car) {
-        return car.getGrossLoad() < MAX_CARRY_WEIGHT;
+        return car != null && car.getGrossLoad() < MAX_CARRY_WEIGHT;
     }
 
     public boolean load(Car car) {
-        if(this.canCarry(car)){
+        if (this.canCarry(car)) {
             this.car = car;
             return true;
         }
         return false;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad() + car.getGrossLoad();
     }
 
     public int getMAX_CARRY_WEIGHT() {
@@ -27,5 +31,9 @@ public class FerryBoat {
 
     public Car getCar() {
         return car;
+    }
+
+    public String toString() {
+        return "FerryBoat{car=" + car.toString() + '}';
     }
 }
