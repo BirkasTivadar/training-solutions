1f3cbfb11c861966dcdb6156c516dac903f585f2
BirkasTivadar <birkastivadar@gmail.com>
2020-12-15 21:49:26 +0100

    Kesz.

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -1,6 +1,8 @@
 package week08d02;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -8,14 +10,24 @@ public class Lottery {
 
     public List<Integer> getNumbers(int interval, int numbers) {
         List<Integer> draws = new ArrayList<>();
-        Random rnd = new Random();
+        Random rnd = new Random(5);
         while (draws.size() < numbers) {
-            Integer nextNumber = rnd.nextInt(interval) + 1;
+            int nextNumber = rnd.nextInt(interval) + 1;
             if (!draws.contains(nextNumber)) {
                 draws.add(nextNumber);
             }
         }
         return draws;
+    }
+
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+
+        List<Integer> list = lottery.getNumbers(90, 5);
+        for(int i =0; i<5; i++){
+
+        System.out.println(list.get(i));
+        }
     }
 }
 
