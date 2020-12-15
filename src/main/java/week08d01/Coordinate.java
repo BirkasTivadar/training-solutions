a67638585f0aa4ed159f921249eec613a506e981
BirkasTivadar <birkastivadar@gmail.com>
2020-12-15 08:25:34 +0100

    Kesz.

--- src/main/java/week08d01/Coordinate.java
+++ src/main/java/week08d01/Coordinate.java
@@ -18,12 +18,14 @@ public class Coordinate {
         this.y = y;
     }
 
+    public void moveRight(){
+        x++;
+    }
+
     public void moveLeft(){
         x--;
     }
-     public void moveRight(){
-        x++;
-    }
+
      public void moveUp(){
         y++;
     }
--- src/main/java/week08d01/Robot.java
+++ src/main/java/week08d01/Robot.java
@@ -7,39 +7,47 @@ public class Robot {
 
     public static final List<Character> CHARS_OF_ORDER = Arrays.asList('B', 'F', 'L', 'J');
 
-    private Coordinate coordinate = new Coordinate(0,0);
+    private Coordinate coordinate = new Coordinate(0, 0);
 
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
-    public boolean isOrder(String str){
-        for(int i=0; i<str.length(); i++){
-            if(!CHARS_OF_ORDER.contains(str.charAt(i))){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public Coordinate move(String orders){
-        if(!isOrder(orders)){
-            throw new IllegalArgumentException("Invalid order.");
-        }
-        for(int i= 0; i<orders.length(); i++){
-            if(orders.charAt(i) == 'B'){
-                coordinate.moveLeft();
-            }
-            if(orders.charAt(i) == 'F'){
-                coordinate.moveUp();
-            }
-            if(orders.charAt(i) == 'L'){
-                coordinate.moveDown();
-            }
-            if(orders.charAt(i) == 'J'){
-                coordinate.moveRight();
-            }
+    public Coordinate move(String orders) {
+        for (int i = 0; i < orders.length(); i++) {
+            oneMoveStep(orders.charAt(i));
+            System.out.println(this.toString());
         }
         return coordinate;
     }
+
+    private void oneMoveStep(Character letter) {
+        switch (letter) {
+            case 'B':
+                coordinate.moveLeft();
+                return;
+            case 'F':
+                coordinate.moveUp();
+                return;
+            case 'L':
+                coordinate.moveDown();
+                return;
+            case 'J':
+                coordinate.moveRight();
+                return;
+            default:
+                throw new IllegalArgumentException("Invalid order.");
+        }
+    }
+
+    public String toString(){
+        return "Robot is in x:"+coordinate.getX() +", y:"+coordinate.getY();
+    }
+
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.move("BFBJBLL");
+
+    }
+
 }
