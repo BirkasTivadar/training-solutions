17387078d614ffe702acc94ff1838bab474dc56e
BirkasTivadar <birkastivadar@gmail.com>
2020-12-16 15:39:46 +0100

    Kesz.

---
+++ src/main/java/week08d03/StringLists.java
@@ -0,0 +1,18 @@
+package week08d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+
+    public List<String> stringListsUnion(List<String> first, List<String> second){
+        List<String> union = new ArrayList<>();
+        union.addAll(first);
+        for(String str: second){
+            if(!union.contains(str)){
+                union.add(str);
+            }
+        }
+        return union;
+    }
+}
