52881ee43f52949d8fa732f3228b1af4643ffe6e
BirkasTivadar <birkastivadar@gmail.com>
2020-12-17 15:43:16 +0100

    Kesz.

---
+++ src/main/java/week08d04/TrainerMain.java
@@ -0,0 +1,14 @@
+package week08d04;
+
+public class TrainerMain {
+
+    public static void main(String[] args) {
+        GoodMood good = new GoodMood();
+
+        Trainer trainerGood = new Trainer(good);
+        Trainer trainerBad = new Trainer(new BadMood());
+
+        System.out.println(trainerGood.giveMark());
+        System.out.println(trainerBad.giveMark());
+    }
+}
