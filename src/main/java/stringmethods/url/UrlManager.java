eb4d0c20679dab7a06e9cade85b56d2f2c859265
BirkasTivadar <birkastivadar@gmail.com>
2020-12-23 19:12:20 +0100

    Kesz.

--- src/main/java/stringmethods/url/UrlManager.java
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -8,11 +8,39 @@ public class UrlManager {
     private String query;
 
     public UrlManager(String url) {
-        this.protocol = protocol;
-        this.port = port;
-        this.host = host;
-        this.path = path;
-        this.query = query;
+        this.protocol = getProtocolFromUrl(url);
+        this.port = 0;
+        this.host = getHostFromUrl(url);
+        this.path = "";
+        this.query = "";
+    }
+
+    private String getProtocolFromUrl(String url) {
+        if (url.indexOf(":") < 0 || url.substring(0, url.indexOf(":")).isBlank()) {
+            throw new IllegalArgumentException("Invalid url");
+        }
+        return url.substring(0, url.indexOf(":")).toLowerCase();
+    }
+
+    private String getHostFromUrl(String url) {
+        String host = "";
+        int start = url.indexOf(":") + 3;
+        int end = url.indexOf(":", start);
+        if (end < 0) {
+            end = url.indexOf("/", start);
+        }
+        if (end < 0) {
+            host = url.substring(start, end).toLowerCase();
+        }
+        if (isEmpty(host)) {
+            throw new IllegalArgumentException("Invalid url");
+        }
+        return host;
+
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isBlank();
     }
 
     public String getProtocol() {
@@ -31,61 +59,12 @@ public class UrlManager {
         return path;
     }
 
-    public void validateString(String str) {
-        if (str == null || str.isBlank()) {
-            throw new IllegalArgumentException("Parameter must be not empty.");
-        }
+    public String getProperty(String key) {
+        return "";
     }
-
-    public String getProcotolFromUrl(String url) {
-        int end = url.indexOf(":");
-        if (end < 0 || url.substring(0, end).isBlank()) {
-            throw new IllegalArgumentException("Invalid url");
-        }
-        return url.substring(0, end).toLowerCase();
-    }
-
-    private String getHostFromUrl(String url) {
-        String host = "";
-        int start = url.indexOf("://") + 3;
-        int end = url.indexOf(":", start);
-        if (end == -1) {
-            end = url.indexOf("/", start);
-        }
-        if (end == -1) {
-            host = url.substring(start).toLowerCase();
-        }
-        if (host.isBlank()) {
-            throw new IllegalArgumentException("invalid url");
-        }
-        return host;
-    }
-
-    private Integer getPortFromUrl(String url) {
-        int start = url.indexOf("://") + 3;
-        start = url.indexOf(":", start);
-        if (start == -1) {
-            return null;
-        }
-        int end = url.indexOf("/", start);
-        if (end == -1) {
-            return Integer.valueOf(url.substring(start + 1));
-        }
-        return Integer.valueOf(url.substring(start+1, end));
-    }
-
-
 
     public boolean hasProperty(String key) {
-        validateString(key);
-        return query.startsWith(key + "=") || query.contains("&" + key + "=");
+        return true;
+
     }
-/*
-    public String getProperty(String key){
-        validateString(key);
-        for(String str : properties)
-
-    }*/
-
-// https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02
 }
