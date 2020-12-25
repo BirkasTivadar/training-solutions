8f37ea3b8727399ea9846338f9d79a95e274d01a
BirkasTivadar <birkastivadar@gmail.com>
2020-12-25 10:21:28 +0100

    Kesz.

--- src/main/java/filescanner/library/Library.java
+++ src/main/java/filescanner/library/Library.java
@@ -1,5 +1,28 @@
 package filescanner.library;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void loadFromFile() {
+        try (Scanner scanner = new Scanner(Library.class.getResourceAsStream("/books.csv")).useDelimiter(";|(\r\n)")) {
+            while (scanner.hasNextLine()) {
+                String regNum = scanner.next();
+                String author = scanner.next();
+                String title = scanner.next();
+                int year = scanner.nextInt();
+                Book book = new Book(regNum,author,title,year);
+                books.add(book);
+            }
+        }
+    }
+
 
 }
