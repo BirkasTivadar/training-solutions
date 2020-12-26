655bfde2f52713d82a8e48bffa44ddd60fe36e36
BirkasTivadar <birkastivadar@gmail.com>
2020-12-26 02:47:00 +0100

    Kesz.

--- src/main/java/algorithmscount/TransactionCounter.java
+++ src/main/java/algorithmscount/TransactionCounter.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class TransactionCounter {
 
-    public int countryLessThan(List<Transaction> transactions, int maxCredit) {
+    public int countEntryLessThan(List<Transaction> transactions, int maxCredit) {
         int counter = 0;
         for (Transaction transaction : transactions) {
             if (transaction.getAmount() < maxCredit) {
