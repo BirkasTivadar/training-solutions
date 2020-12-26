7795a4c448b3e142d449985e7f7ad88b12f8a9bc
BirkasTivadar <birkastivadar@gmail.com>
2020-12-26 15:36:05 +0100

    Kesz.

--- src/main/java/algorithmsdecision/TransactionExecutor.java
+++ src/main/java/algorithmsdecision/TransactionExecutor.java
@@ -1,5 +1,10 @@
 package algorithmsdecision;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 public class TransactionExecutor {
@@ -11,8 +16,7 @@ public class TransactionExecutor {
                     if (transaction.isCredit()) {
                         account.deposit(transaction.getAmount());
                         transaction.statusChangeSucceded();
-                    }
-                    if (transaction.isDebit() && account.withdraw(transaction.getAmount())) {
+                    } else if (transaction.isDebit() && account.withdraw(transaction.getAmount())) {
                         transaction.statusChangeSucceded();
                     } else {
                         transaction.statusChangePending();
@@ -20,5 +24,25 @@ public class TransactionExecutor {
                 }
             }
         }
+    }
+
+
+    public static void main(String[] args) {
+        List<BankAccount> accounts = Arrays.asList(
+                new BankAccount("Fred", "123-324-123", 100),
+                new BankAccount("Kate", "133-324-123", 2000),
+                new BankAccount("Mary", "143-324-123", 90)
+        );
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction("123-324-123", TransactionOperation.CREDIT, 2000, LocalDateTime.MIN),
+                new Transaction("133-324-123", TransactionOperation.DEBIT, 200, LocalDateTime.MIN),
+                new Transaction("143-324-123", TransactionOperation.DEBIT, 2000, LocalDateTime.MIN)
+        );
+
+        TransactionExecutor transactionExecutor = new TransactionExecutor();
+
+        transactionExecutor.executeTransactions(transactions, accounts);
+        System.out.println(accounts.get(0).getBalance());
     }
 }
