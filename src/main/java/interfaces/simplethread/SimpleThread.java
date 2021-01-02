77f8327e2f7da37850a0a1c2747754a875ab4ff9
BirkasTivadar <birkastivadar@gmail.com>
2021-01-02 10:02:01 +0100

    Kesz.

--- src/main/java/interfaces/simplethread/SimpleThread.java
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -7,18 +7,18 @@ public class SimpleThread implements Runnable {
     private List<String> tasks;
 
     public SimpleThread(List<String> tasks) {
-        if(tasks == null){
+        if (tasks == null) {
             throw new IllegalArgumentException("Tasks must not be null.");
         }
-        this.tasks = tasks;
+        this.tasks = new ArrayList<>(tasks);
     }
 
     public List<String> getTasks() {
-        return tasks;
+        return List.copyOf(tasks);
     }
 
     private boolean nextStep() {
-        if (!tasks.isEmpty()){
+        if (!tasks.isEmpty()) {
             tasks.remove(tasks.size() - 1);
         }
         return tasks.isEmpty() ? false : true;
