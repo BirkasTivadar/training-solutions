eb84db265be3119d405cc68e11db02c4ca0a55a4
BirkasTivadar <birkastivadar@gmail.com>
2021-01-04 05:56:47 +0100

    Kesz.

---
+++ src/test/java/x/PrintableTest.java
@@ -0,0 +1,15 @@
+package x;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintableTest {
+    @Test
+    public void testGetColorWithDefaultImplementation() {
+        Printable printable = new TestPrintable();
+        assertEquals(Printable.BLACK, printable.getColor(0));
+    }
+
+
+}
