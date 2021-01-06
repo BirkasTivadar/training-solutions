37228dd950a0f4974a76f719845efaedbfd37dff
BirkasTivadar <birkastivadar@gmail.com>
2021-01-06 19:51:41 +0100

    Kesz.

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -2,6 +2,20 @@ package exam02;
 
 public class ArraySelector {
 
-
+public String selectEvens(int[] numbers){
+    String s = "";
+    if(numbers.length == 0){
+        return s;
+    }
+    s += "[";
+    for(int i=0; i < numbers.length; i++){
+        if(i%2 == 0){
+            s += numbers[i];
+            s += ", ";
+        }
+    }
+    String subString = s.substring(0, s.length()-2);
+    return subString + "]";
+}
 
 }
