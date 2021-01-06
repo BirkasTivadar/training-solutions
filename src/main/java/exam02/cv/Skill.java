bced23d5739460dd5e94d7fd33fa587b3a8b3208
BirkasTivadar <birkastivadar@gmail.com>
2021-01-06 20:33:17 +0100

    Kesz.

--- src/main/java/exam02/cv/Skill.java
+++ src/main/java/exam02/cv/Skill.java
@@ -13,15 +13,9 @@ public class Skill {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getLevel() {
         return level;
     }
 
-    public void setLevel(int level) {
-        this.level = level;
-    }
+
 }
