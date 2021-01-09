622a7a733b5e005bb9716447713ede498dd17bc0
BirkasTivadar <birkastivadar@gmail.com>
2021-01-09 16:23:16 +0100

    Kesz.

--- src/main/java/catalog/CatalogItem.java
+++ src/main/java/catalog/CatalogItem.java
@@ -1,6 +1,7 @@
 package catalog;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CatalogItem {
@@ -8,9 +9,30 @@ public class CatalogItem {
     private int price;
     private String registrationNumber;
 
-    public CatalogItem(String registrationNumber, int price, List<Feature> features){
+    public CatalogItem(String registrationNumber, int price, Feature... features) {
+        this.features = Arrays.asList(features);
         this.registrationNumber = registrationNumber;
         this.price = price;
-        this.features = List.copyOf(features);
+
+    }
+
+    public List<Feature> getFeatures() {
+        return List.copyOf(features);
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public List<String> getContributors() {
+        List<String> result = new ArrayList<>();
+        for (Feature feature : features) {
+            result.addAll(feature.getContributors());
+        }
+        return result;
     }
 }
