af07115358067c079793688dae33fc3149beec14
BirkasTivadar <birkastivadar@gmail.com>
2021-01-10 17:26:27 +0100

    Kesz.

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -5,34 +5,27 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    private int findMinSum(int[] arr) {
+    private MinMax findMinMaxSumMoreFour(int[] arr) {
         Integer[] four = {arr[0], arr[1], arr[2], arr[3]};
-        int sum = arr[0] + arr[1] + arr[2] + arr[3];
+        int sumMax = arr[0] + arr[1] + arr[2] + arr[3];
+        int sumMin = arr[0] + arr[1] + arr[2] + arr[3];
         int max = Math.max(Math.max(four[0], four[1]), Math.max(four[2], four[3]));
-        int sumMinusMax = sum - max;
+        int min = Math.min(Math.min(four[0], four[1]), Math.min(four[2], four[3]));
+        int sumMinusMax = sumMax - max;
+        int sumMinusMin = sumMin - min;
         for (int i = 4; i < arr.length; i++) {
-            if (sumMinusMax + arr[i] < sum) {
-                sum = sumMinusMax + arr[i];
+            if (sumMinusMax + arr[i] < sumMax) {
+                sumMax = sumMinusMax + arr[i];
                 Array.set(four, Arrays.asList(four).indexOf(max), arr[i]);
                 max = Math.max(Math.max(four[0], four[1]), Math.max(four[2], four[3]));
             }
-        }
-        return sum;
-    }
-
-    private int findMaxSum(int[] arr) {
-        Integer[] four = {arr[0], arr[1], arr[2], arr[3]};
-        int sum = arr[0] + arr[1] + arr[2] + arr[3];
-        int min = Math.min(Math.min(four[0], four[1]), Math.min(four[2], four[3]));
-        int sumMinusMin = sum - min;
-        for (int i = 4; i < arr.length; i++) {
-            if (sumMinusMin + arr[i] > sum) {
-                sum = sumMinusMin + arr[i];
+            if (sumMinusMin + arr[i] > sumMin) {
+                sumMin = sumMinusMin + arr[i];
                 Array.set(four, Arrays.asList(four).indexOf(min), arr[i]);
                 min = Math.min(Math.min(four[0], four[1]), Math.min(four[2], four[3]));
             }
         }
-        return sum;
+        return new MinMax(sumMax, sumMin);
     }
 
     public MinMax findMinMaxSum(int[] arr) {
@@ -46,7 +39,7 @@ public class Calculator {
             }
             return new MinMax(sum, sum);
         }
-        return new MinMax(findMinSum(arr), findMaxSum(arr));
+        return findMinMaxSumMoreFour(arr);
     }
 
 }
