7b9895fc59c925352b77fa49f914671960910052
BirkasTivadar <birkastivadar@gmail.com>
2021-01-10 16:46:26 +0100

    Kesz.

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public int findMinMaxSum(int[] arr) {
+    private int findMinSum(int[] arr) {
         if(arr.length < 4){
             int sum = 0;
             for(int number : arr){
@@ -26,4 +26,31 @@ public class Calculator {
         }
         return sum;
     }
+
+     private int findMaxSum(int[] arr) {
+        if(arr.length < 4){
+            int sum = 0;
+            for(int number : arr){
+                sum += number;
+            }
+            return sum;
+        }
+        int[] four = new int[]{arr[0], arr[1], arr[2], arr[3]};
+        int sum = arr[0] + arr[1] + arr[2] + arr[3];
+        int min = Math.min(Math.min(arr[0], arr[1]), Math.min(arr[2], arr[3]));
+        int sumMinusMin = sum - min;
+        for (int i = 4; i < arr.length; i++) {
+            if (sumMinusMin + arr[i] > sum) {
+                sum = sumMinusMin + arr[i];
+                Array.set(four, Arrays.asList(four).indexOf(min), arr[i]);
+                min = Math.min(Math.min(four[0], four[1]), Math.min(four[2], four[3]));
+            }
+        }
+        return sum;
+    }
+
+    public MinMax indMinMaxSum(int[] arr){
+        return new MinMax(findMinSum(arr), findMaxSum(arr));
+    }
+
 }
