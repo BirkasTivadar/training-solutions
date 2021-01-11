4e08e6ece40cf687b7716be8e7ad14efec668ac8
BirkasTivadar <birkastivadar@gmail.com>
2021-01-11 18:45:09 +0100

    Kivételkezelés videós feladat

---
+++ src/main/java/exceptions/NumbersReader.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import java.util.Scanner;
+
+public class NumbersReader {
+
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner((NumbersReader.class.getResourceAsStream("numbers.txt")));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                int number = Integer.parseInt(line);
+                System.out.println(number);
+            }
+        } catch (NumberFormatException nfe) {
+            // System.out.println("Invalid number.");
+            throw new IllegalStateException("Invalid number.", nfe);
+        } catch (NullPointerException e) {
+            throw e;
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
