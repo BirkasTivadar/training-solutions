af9a75a6938775de01a90e22015f080e9799925e
BirkasTivadar <birkastivadar@gmail.com>
2021-01-13 18:29:03 +0100

    Week11d03 WordFilter.

--- src/test/java/week11d03/WordFilterTest.java
+++ src/test/java/week11d03/WordFilterTest.java
@@ -2,11 +2,25 @@ package week11d03;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordFilterTest {
 
     @Test
     void wordsWithChar() {
+        WordFilter wordFilter = new WordFilter();
+        List<String> words = new ArrayList<>();
+        words.add("kutya");
+        words.add("macska");
+        words.add("körte");
+        words.add("egér");
+        words.add("ablak");
+
+        assertEquals(3, wordFilter.wordsWithChar(words, 'a').size());
+        assertEquals(2, wordFilter.wordsWithChar(words, 'e').size());
+        assertEquals(0, wordFilter.wordsWithChar(words, 'ü').size());
     }
 }
