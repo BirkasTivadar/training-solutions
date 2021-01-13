577bf8e9d4be0e590e1a4091c02ad6c68afa440b
BirkasTivadar <birkastivadar@gmail.com>
2021-01-13 15:40:44 +0100

    Week11d03 WordFilter.

--- src/main/java/week11d03/WordFilter.java
+++ src/main/java/week11d03/WordFilter.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class WordFilter {
 
     public List<String> wordsWithChar(List<String> words, char c) {
+        if(words == null || words.size() < 1 || String.valueOf(c) == ""){
+            throw new IllegalArgumentException("Invalid data");
+        }
         List<String> result = new ArrayList<>();
         String filter = String.valueOf(c);
         for (String word : words) {
