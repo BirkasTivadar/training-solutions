55ddaff5237cead0597f922de1c180b7ac604b11
BirkasTivadar <birkastivadar@gmail.com>
2021-01-15 09:03:53 +0100

    Ioreadwrite Temperatures elvileg kész.

--- src/main/java/ioreadwritebytes/Temperatures.java
+++ src/main/java/ioreadwritebytes/Temperatures.java
@@ -2,7 +2,7 @@ package ioreadwritebytes;
 
 public class Temperatures {
 
-    private byte [] data;
+    private byte[] data;
 
     public Temperatures(byte[] data) {
         this.data = data;
@@ -10,5 +10,21 @@ public class Temperatures {
 
     public byte[] getData() {
         return data;
+    }
+
+    public double getYearAverage() {
+        double sum = 0;
+        for (byte temperatureOfOneDay : data) {
+            sum += temperatureOfOneDay;
+        }
+        return sum / data.length;
+    }
+
+    public double getMonthAverage() {
+        double sum = 0;
+        for (int i = data.length - 30; i < data.length; i++) {
+            sum += data[i];
+        }
+        return sum / 30;
     }
 }
