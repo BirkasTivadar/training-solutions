4dcaa1828bf1e70e0fa23bd19321f1a6d47c7056
BirkasTivadar <birkastivadar@gmail.com>
2021-01-15 11:02:02 +0100

    Ioreader IdManager  kész.

--- src/main/java/x/EmployeeService.java
+++ src/main/java/x/EmployeeService.java
@@ -1,22 +1,23 @@
 package x;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class EmployeeService {
 
     public static void main(String[] args) {
         Path file = Path.of("employees.txt");
-        List<String> content = null;
-        try {
-            content = Files.readAllLines(file);
-        } catch (IOException e) {
-            throw new IllegalStateException("Can not read file", e);
-        }
-        System.out.println(content);
-    }
 
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read this file:" + file.toString());
+        }
+    }
 }
