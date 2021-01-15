7d3a5546856bb02d91eb7c0f312ce194e86dcca7
BirkasTivadar <birkastivadar@gmail.com>
2021-01-15 08:16:50 +0100

    Iowritestring Diary kész. A feladatkiírás alapján. Így viszont nem futnak a tesztesetek, mert ott kicsit eltérő megoldást vár.

--- src/main/java/iowritestring/school/Diary.java
+++ src/main/java/iowritestring/school/Diary.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class Diary {
 
@@ -20,12 +21,32 @@ public class Diary {
         } catch (IOException e) {
             e.printStackTrace();
 
-        }}
-
-        public static void main (String[]args){
-            Diary diary = new Diary();
-            diary.newMark("Bibi", 4);
-            diary.newMark("Bibi", 5);
-            diary.newMark("Jimmy", 2);
         }
     }
+
+    public void average(String name) {
+        String fileName = name + ".txt";
+        Path file = Path.of("src", "main", "resources", fileName);
+        try {
+            List<String> marks = Files.readAllLines(file);
+            double sum = 0;
+            for (String mark : marks) {
+                sum += Double.parseDouble(mark);
+            }
+            Files.writeString(file, "average: " + sum / marks.size(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read this file");
+        }
+    }
+
+    public static void main(String[] args) {
+        Diary diary = new Diary();
+        diary.newMark("Bibi", 4);
+        diary.newMark("Bibi", 5);
+        diary.newMark("Jimmy", 2);
+        diary.newMark("Jimmy", 1);
+        diary.newMark("Bibi", 2);
+        diary.average("Jimmy");
+        diary.average("Bibi");
+    }
+}
