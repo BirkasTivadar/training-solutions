58b45479b9655cc6db4eb97114c0bf6255d7d2f0
BirkasTivadar <birkastivadar@gmail.com>
2021-01-15 08:57:17 +0100

    Ioreadwrite Temperatures v1.

---
+++ src/main/java/ioreadwritebytes/Temperatures.java
@@ -0,0 +1,14 @@
+package ioreadwritebytes;
+
+public class Temperatures {
+
+    private byte [] data;
+
+    public Temperatures(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+}
