a79a2f498e873b24bf8d779caefd594d992e5bfa
BirkasTivadar <birkastivadar@gmail.com>
2021-01-16 15:00:07 +0100

    InputStream ByteChecker feladat elvileg kész.
    Kérdés, hogy ha a buffer mértetét 100-ra leveszem miért nem működik?

---
+++ src/main/java/ioreadbytes/byteacount/ByteChecker.java
@@ -0,0 +1,30 @@
+package ioreadbytes.byteacount;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ByteChecker {
+
+    public int readBytesAndFindAs(String fileName) {
+        Path path = Path.of(fileName);
+        int result = 0;
+        try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(path))) {
+            byte[] bytes = new byte[1000];
+            while ((inputStream.read(bytes)) > 0) {
+                for (byte a : bytes) {
+                    if (a == 97) {
+                        result++;
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read this file.", ioe);
+        }
+        return result;
+    }
+
+
+}
