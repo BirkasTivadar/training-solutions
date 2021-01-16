3a021b3d61b1087c86f1e267675ec9fc7fca22a8
BirkasTivadar <birkastivadar@gmail.com>
2021-01-16 14:01:41 +0100

    IoPrintWriter Salary feladat kész. IoStringWriter LongWordService feladat kész.

---
+++ src/main/java/ioprintwriter/salary/SalaryWriter.java
@@ -0,0 +1,39 @@
+package ioprintwriter.salary;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalaryWriter {
+
+    private List<String> names;
+
+    public SalaryWriter(List<String> names) {
+        this.names = new ArrayList<>(names);
+    }
+
+    private int salary(String name) {
+        if (name.contains("Dr.")) {
+            return 500_000;
+        } else if (name.contains("Mrs.")) {
+            return 200_000;
+        } else {
+            return 100_000;
+        }
+    }
+
+    public void writeNamesAndSalaries(Path path) {
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path))) {
+            for (String name : names) {
+                writer.print(name);
+                writer.print(": ");
+                writer.println(salary(name));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write this file: " + path);
+        }
+    }
+}
---
+++ src/main/java/iostringwriter/longwords/LongWordService.java
@@ -0,0 +1,28 @@
+package iostringwriter.longwords;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+public class LongWordService {
+
+    public String writeWithStringWriter(Writer writer, List<String> words) {
+        try (writer) {
+            for (String word : words) {
+                writer.write(word);
+                writer.write(" ");
+                writer.write("" + word.length());
+                writer.write("\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not close this writer.");
+        }
+        return writer.toString();
+    }
+
+    public String writeWithStringWriter(List<String> words) {
+        StringWriter sw = new StringWriter();
+        return writeWithStringWriter(sw, words);
+    }
+}
