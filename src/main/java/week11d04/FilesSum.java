4035b2030f6c379ab070e1f0e8692c47a790c8c3
BirkasTivadar <birkastivadar@gmail.com>
2021-01-17 21:12:38 +0100

    Week11d04 FileSum v0.1

--- src/main/java/week11d04/FilesSum.java
+++ src/main/java/week11d04/FilesSum.java
@@ -1,29 +1,32 @@
 package week11d04;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class FilesSum {
 
-    private int readFile(Path path) {
+    private int sumNumbers() {
+
         int number = 0;
-        if (Files.isRegularFile(path)) {
-            try {
-                String content = Files.readString(path);
-                try {
-                    number = Integer.parseInt(content);
-                } catch (NumberFormatException nfe) {
-                    System.out.println("");
-                }
-            } catch (IOException ioe) {
-                throw new IllegalStateException("Can not read file", ioe);
-            }
-        }
-        return number;
+return 0;
     }
 
 
+    private String createFilename(int i) {
+        if (i < 10) {
+            return "number0" + i + ".txt";
+        } else {
+            return "number" + i + ".txt";
+        }
+    }
 
+    public static void main(String[] args) {
+        FilesSum filesSum = new FilesSum();
+        System.out.println(filesSum.createFilename(1));
+        System.out.println(filesSum.createFilename(31));
+    }
 }
