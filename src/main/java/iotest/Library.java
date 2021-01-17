863decfc5632943bfc9ca0288c80b51825ecca27
BirkasTivadar <birkastivadar@gmail.com>
2021-01-17 20:14:52 +0100

    Iotest Library feladat v0.3

--- src/main/java/iotest/Library.java
+++ src/main/java/iotest/Library.java
@@ -1,5 +1,9 @@
 package iotest;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +11,19 @@ public class Library {
 
     private List<Book> books = new ArrayList<>();
 
-    public void add(Book... books){
-        for(Book book : books){
+    public void add(Book... books) {
+        for (Book book : books) {
             this.books.add(book);
+        }
+    }
+
+    public void saveBooks(Path file) {
+        try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+            for (Book book : books) {
+                writer.write(book.getAuthor() + ": " + book.getTitle() + "\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write", ioe);
         }
     }
 }
