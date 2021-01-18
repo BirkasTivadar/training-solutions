79592777e8ff03cfac7499d2cd2db57b5b6416fb
BirkasTivadar <birkastivadar@gmail.com>
2021-01-18 15:49:18 +0100

    Week12d01 GradeRounder kész.

---
+++ src/main/java/week12d01/GradeRounder.java
@@ -0,0 +1,22 @@
+package week12d01;
+
+public class GradeRounder {
+
+    public int[] roundGrades(int[] grades) {
+        int size = grades.length;
+        int[] result = new int[size];
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] <= 40) {
+                result[i] = grades[i];
+            } else {
+                int remain = grades[i] % 5;
+                if(remain < 3){
+                    result[i] = (grades[i]/5)*5;
+                } else {
+                    result[i] = (grades[i]/5)*6;
+                }
+            }
+        }
+        return result;
+    }
+}
