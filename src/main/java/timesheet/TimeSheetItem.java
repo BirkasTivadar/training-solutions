1cda33fb1925ab94d8b865e1fc1fb96c999398bd
BirkasTivadar <birkastivadar@gmail.com>
2021-01-20 23:03:28 +0100

    5. projektfeladat TimeSheet v0.1

---
+++ src/main/java/timesheet/TimeSheetItem.java
@@ -0,0 +1,33 @@
+package timesheet;
+
+import java.time.LocalDateTime;
+
+public class TimeSheetItem {
+    private final Employee employee;
+    private final Project project;
+    private final LocalDateTime beginDate;
+    private final LocalDateTime endDate;
+
+    public TimeSheetItem(Employee employee, Project project, LocalDateTime beginDate, LocalDateTime endDate) {
+        this.employee = employee;
+        this.project = project;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public LocalDateTime getBeginDate() {
+        return beginDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+}
