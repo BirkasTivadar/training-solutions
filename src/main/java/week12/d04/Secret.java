2227b59a2e5b37ae178e7ec37bfec9260bfd7a4d
BirkasTivadar <birkastivadar@gmail.com>
2021-01-21 15:52:09 +0100

    5. projektfeladat TimeSheet v0.1

---
+++ src/main/java/week12/d04/Secret.java
@@ -0,0 +1,31 @@
+package week12.d04;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Secret {
+
+
+    public void printSecret() {
+        Path file = Path.of("secret.dat");
+
+        try (InputStream is = Files.newInputStream(file)) {
+            byte[] bytes = is.readAllBytes();
+            for(int i=0; i< bytes.length ; i++){
+                bytes[i] += 10;
+            }
+            for(int i=0; i< bytes.length ; i++){
+                System.out.print((char)bytes[i]);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read.", ioe);
+        }
+    }
+
+    public static void main(String[] args) {
+        Secret secret = new Secret();
+        secret.printSecret();
+    }
+}
