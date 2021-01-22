7abea101111dee6111a46e9a90a17d168a4aab65
BirkasTivadar <birkastivadar@gmail.com>
2021-01-22 20:04:27 +0100

    Exceptionclass Bank feladat kész

---
+++ src/main/java/exceptionclass/bank/Bank.java
@@ -0,0 +1,38 @@
+package exceptionclass.bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+
+    private List<Account> accounts = new ArrayList<>();
+
+    public Bank(List<Account> accounts) {
+        if (accounts == null) {
+            throw new IllegalArgumentException();
+        }
+        this.accounts = accounts;
+    }
+
+    public void payment(String accountNumber, double amount) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                account.subtract(amount);
+                return;
+            }
+        }
+        throw new InvalidBankOperationException(ErrorCode.INVALID_ACCOUNTNUMBER);
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                account.deposit(amount);
+                return;
+            }
+        }
+        throw new InvalidBankOperationException(ErrorCode.INVALID_ACCOUNTNUMBER);
+    }
+
+
+}
