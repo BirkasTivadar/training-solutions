43fc50c00e506a3c593a4886418186d123d490a6
BirkasTivadar <birkastivadar@gmail.com>
2021-01-24 20:01:53 +0100

    IoZip Napi Tranzakciók feladat v0.5

--- src/main/java/iozip/transactions/TransactionFileManager.java
+++ src/main/java/iozip/transactions/TransactionFileManager.java
@@ -1,4 +1,26 @@
 package iozip.transactions;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class TransactionFileManager {
+
+    public void saveTransactions(Path zip, List<Transaction> transactionList) {
+        try (ZipOutputStream outputStream = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(zip)))) {
+            for (Transaction transaction : transactionList){
+                outputStream.putNextEntry(new ZipEntry(transaction.getId()+".dat"));
+                outputStream.write((transaction.getTime().toString()+"\n").getBytes());
+                outputStream.write((transaction.getAccount()+"\n").getBytes());
+                outputStream.write(String.valueOf(transaction.getAmount()).getBytes());
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write.", ioe);
+        }
+
+    }
 }
