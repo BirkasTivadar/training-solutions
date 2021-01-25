99d3b5127a62d9e9a7f42f53cf7da995951c1b26
BirkasTivadar <birkastivadar@gmail.com>
2021-01-25 15:46:44 +0100

    week13d01 citySearch kész.

---
+++ src/main/java/week13d01/CitySearch.java
@@ -0,0 +1,29 @@
+package week13d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+
+public class CitySearch {
+
+    public String search() {
+        int max = 0;
+        String longestName = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(CitySearch.class.getResourceAsStream("iranyitoszamok-varosok-2021.csv")))) {
+            String line;
+            line = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] cn = line.split(";");
+                String name = cn[1].trim();
+                if (name.length() > max) {
+                    max = name.length();
+                    longestName = name;
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read", ioe);
+        }
+        return longestName;
+    }
+}
