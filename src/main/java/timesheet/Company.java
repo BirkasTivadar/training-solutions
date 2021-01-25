f106ad9b33a6aabb6a18ffc636ba95be83f1a0f7
BirkasTivadar <birkastivadar@gmail.com>
2021-01-25 13:29:52 +0100

    5. projektfeladat TimeSheet v0.2

---
+++ src/main/java/timesheet/Company.java
@@ -0,0 +1,5 @@
+package timesheet;
+
+public class Company {
+    //private
+}
---
+++ src/main/java/timesheet/ReportLine.java
@@ -0,0 +1,24 @@
+package timesheet;
+
+public class ReportLine {
+
+    private Project project;
+    private long time;
+
+    public ReportLine(Project project, long time) {
+        this.project = project;
+        this.time = time;
+    }
+
+    public void addTime(long hours){
+        time += hours;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public long getTime() {
+        return time;
+    }
+}
