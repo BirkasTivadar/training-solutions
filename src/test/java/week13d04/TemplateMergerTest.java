2eca65bcf302d6d8f44c40f498456c8e39388472
BirkasTivadar <birkastivadar@gmail.com>
2021-01-28 19:03:58 +0100

    week13d04 TemplateMerge teszttel együtt kész.

--- src/test/java/week13d04/TemplateMergerTest.java
+++ src/test/java/week13d04/TemplateMergerTest.java
@@ -12,13 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class TemplateMergerTest {
 
     @Test
-    void testTemplateMerge(){
+    void testTemplateMerge() {
         Path file = Path.of("blank.txt");
         Employee john = new Employee("John Doe", 1990);
         Employee jack = new Employee("Jack Doe", 2000);
         List<Employee> employees = new ArrayList<>(Arrays.asList(john, jack));
         TemplateMerger templateMerger = new TemplateMerger();
-        System.out.println(templateMerger.merge(file, employees));
+        assertEquals("Az alkalmazott neve: John Doe, születési éve: 1990\n" +
+                "Az alkalmazott neve: Jack Doe, születési éve: 2000\n", templateMerger.merge(file, employees));
     }
 
 }
