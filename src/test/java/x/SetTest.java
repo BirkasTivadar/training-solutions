e4a777a7611d50b6abb8f2f642e4be06f1dd1f71
BirkasTivadar <birkastivadar@gmail.com>
2021-01-28 19:19:58 +0100

    Set gyakorlás

---
+++ src/test/java/x/SetTest.java
@@ -0,0 +1,41 @@
+package x;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SetTest {
+
+    @Test
+    void testSet() {
+        Set<Trainer> s = new HashSet<>();
+        Collection<Trainer> c = s;
+        c.add(new Trainer("Alma"));
+        c.add(new Trainer("Alma"));
+        c.add(new Trainer("Körte"));
+
+        assertEquals(2, s.size());
+        System.out.println(s.toString());
+    }
+
+    @Test
+    void testSetOrder() {
+        Set<Trainer> s = new LinkedHashSet<>();
+        Collection<Trainer> c = s;
+        c.add(new Trainer("Jane"));
+        c.add(new Trainer("Jack"));
+        c.add(new Trainer("Jack"));
+
+        System.out.println(s.toString());
+
+        for (Trainer t : s) {
+            System.out.println(t.getName());
+        }
+    }
+
+}
