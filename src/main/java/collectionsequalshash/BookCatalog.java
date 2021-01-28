9c939ea354549055a810a9882d03de491b41ec64
BirkasTivadar <birkastivadar@gmail.com>
2021-01-28 09:51:25 +0100

    equals és hashCode metódusok kipróbálása v0.1

---
+++ src/main/java/collectionsequalshash/BookCatalog.java
@@ -0,0 +1,24 @@
+package collectionsequalshash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCatalog {
+    private List<Book> books = new ArrayList<>();
+
+
+
+    public List<Book> getBooks() {
+        return new ArrayList<>(books);
+    }
+
+    public Book findBookByTitleAuthor(List<Book> books, String title, String author){
+        Book searchBook = new Book("1", title, author);
+        for(Book book :books){
+            if(book.equals(searchBook)){
+                return book;
+            }
+        }
+        return null;
+    }
+}
