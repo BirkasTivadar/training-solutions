3c3a35bbdb505f0dcc17984275a59e33b66149e6
BirkasTivadar <birkastivadar@gmail.com>
2021-01-28 11:57:20 +0100

    List, ArrayList kontra LinkedList Lottó1 feladat kész.

---
+++ src/main/java/collectionslist/collectionsarraylist/Lottery.java
@@ -0,0 +1,23 @@
+package collectionslist.collectionsarraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lottery {
+
+    public List<Integer> selectWinningNumbers(int lotteryType, int ballCount) {
+        if (lotteryType >= ballCount) {
+            throw new IllegalArgumentException("Balls must be more then the winning numbers!");
+        }
+        List<Integer> result = new ArrayList<>();
+        Random rnd = new Random();
+        while (result.size() < lotteryType) {
+            int oneBall = rnd.nextInt(ballCount) + 1;
+            if (!result.contains(oneBall)) {
+                result.add(oneBall);
+            }
+        }
+        return result;
+    }
+}
