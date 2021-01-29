3b8e7dc155ecfc89027dc9dd057c8e8348133c58
BirkasTivadar <birkastivadar@gmail.com>
2021-01-29 12:57:58 +0100

    Autoboxing használata kollekcióknál, Az autoboxing működése Integer objektumok összeadása és kollekció esetében feladat kész.

---
+++ src/test/java/x/IntegerOperationTest.java
@@ -0,0 +1,16 @@
+package x;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntegerOperationTest {
+    @Test
+    void testAutoBoxing(){
+        List<Integer> l = new ArrayList<>();
+        l.add(5);
+    }
+}
