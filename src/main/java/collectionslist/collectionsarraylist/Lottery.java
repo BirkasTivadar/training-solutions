bece2731b41228e569f90b1da04ba5c6ee42dc43
BirkasTivadar <birkastivadar@gmail.com>
2021-02-05 12:04:32 +0100

    Javítás

--- src/main/java/collectionslist/collectionsarraylist/Lottery.java
+++ src/main/java/collectionslist/collectionsarraylist/Lottery.java
@@ -3,23 +3,18 @@ package collectionslist.collectionsarraylist;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Lottery {
 
     public List<Integer> selectWinningNumbers(int lotteryType, int ballCount) {
-        if (lotteryType >= ballCount) {
+        if (ballCount <= lotteryType) {
             throw new IllegalArgumentException("Balls must be more then the winning numbers!");
         }
         List<Integer> result = new ArrayList<>();
-        Random rnd = new Random();
-        while (result.size() < lotteryType) {
-            int oneBall = rnd.nextInt(ballCount) + 1;
-            if (!result.contains(oneBall)) {
-                result.add(oneBall);
-            }
+        for (int i = 1; i <= ballCount; i++) {
+            result.add(i);
         }
-        Collections.sort(result);
-        return result ;
+        Collections.shuffle(result);
+        return result.subList(0, lotteryType);
     }
 }
