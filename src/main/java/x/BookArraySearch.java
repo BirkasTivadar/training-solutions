364d1bf3c3f91cae9d0e1c54c4bc4b025990a9f1
BirkasTivadar <birkastivadar@gmail.com>
2021-02-06 19:05:50 +0100

    Ismétlés

--- src/main/java/x/BookArraySearch.java
+++ src/main/java/x/BookArraySearch.java
@@ -11,13 +11,13 @@ public class BookArraySearch {
     }
 
     public Book findBookByAuthorTitle(String author, String title) {
-        if (author == null || title == null || author.isBlank() || title.isBlank()){
+        if (author == null || title == null || author.isBlank() || title.isBlank()) {
             throw new IllegalArgumentException("Author or title must not be empty!");
         }
         Arrays.sort(bookArray);
         Book searchedBook = new Book(author, title);
         int index = Arrays.binarySearch(bookArray, searchedBook);
-        if(index<0){
+        if (index < 0) {
             throw new IllegalArgumentException("No book found by " + author + " with title " + title);
         }
         return bookArray[index];
