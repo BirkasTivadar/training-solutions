3e81a8f96c6aa2d9e44e50f0df201ffc8bc8844a
BirkasTivadar <birkastivadar@gmail.com>
2021-02-06 09:35:36 +0100

    Ismétlés

---
+++ src/main/java/x/NoJobException.java
@@ -0,0 +1,10 @@
+package x;
+
+public class NoJobException extends Exception {
+    public NoJobException() {
+    }
+
+    public NoJobException(String message) {
+        super(message);
+    }
+}
