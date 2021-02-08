b77eabbc2d1ffe09eaa74d6306c0a719f4c2b034
BirkasTivadar <birkastivadar@gmail.com>
2021-02-08 21:18:01 +0100

    week15d01 Coordinates feladat javítás

--- src/main/java/week15d01/Coordinates.java
+++ src/main/java/week15d01/Coordinates.java
@@ -5,6 +5,9 @@ import java.util.Map;
 public class Coordinates {
 
     public Map.Entry<Double, Double> searchMax(Map<Double, Double> coordinates) {
+        if(coordinates ==  null || coordinates.isEmpty()){
+            throw new IllegalArgumentException("Coordinates must not be empty");
+        }
         Map.Entry<Double, Double> result = null;
         for (Map.Entry<Double, Double> entry : coordinates.entrySet()) {
             if (result == null || entry.getValue() > result.getValue()) {
---
+++ src/test/java/week15d01/CoordinatesTest.java
@@ -0,0 +1,28 @@
+package week15d01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinatesTest {
+
+    @Test
+    void testCoordinates() {
+        Coordinates coordinates = new Coordinates();
+        Map<Double, Double> testMap = new HashMap<>();
+        testMap.put(2.3, 4.5);
+        testMap.put(6.0, 2.7);
+        testMap.put(2.8, -3.5);
+        testMap.put(4.2, 5.5);
+        testMap.put(2.6, 4.7);
+        Map.Entry<Double, Double> testEntry = coordinates.searchMax(testMap);
+
+        assertEquals(4.2, testEntry.getKey());
+        assertEquals(5.5, testEntry.getValue());
+    }
+
+}
