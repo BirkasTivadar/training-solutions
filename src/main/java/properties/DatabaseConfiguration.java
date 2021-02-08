84eb47c23efbcc47c5458f7e5acf2bc9136d183c
BirkasTivadar <birkastivadar@gmail.com>
2021-02-08 21:51:06 +0100

    Properties állományok adatbázis konfiguráció beolvasása feladat 0.5

---
+++ src/main/java/properties/DatabaseConfiguration.java
@@ -0,0 +1,35 @@
+package properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DatabaseConfiguration {
+    private String host;
+    private String port;
+    private String schema;
+
+    public DatabaseConfiguration() {
+        Properties properties = new Properties();
+        try (InputStream is = Properties.class.getResourceAsStream("/db.properties")) {
+            properties.load(is);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot load file", ioe);
+        }
+        this.host = properties.getProperty("host");
+        this.port = properties.getProperty("port");
+        this.schema = properties.getProperty("schema");
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+}
