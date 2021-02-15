47b73ccc7e09a24c2a9151c73549aade7f3024d1
BirkasTivadar <birkastivadar@gmail.com>
2021-02-15 19:02:32 +0100

    Histogram

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,26 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader br = reader) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int counter = Integer.parseInt(line);
+                for (int i = 1; i <= counter; i++) {
+                    str.append("*");
+                }
+                str.append("\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read");
+        }
+        return str.toString();
+    }
+
 }
