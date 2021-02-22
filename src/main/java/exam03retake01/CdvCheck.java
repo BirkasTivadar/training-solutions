85d8940fd12ca116c679128f60cad2d693a75b34
BirkasTivadar <birkastivadar@gmail.com>
2021-02-22 18:01:12 +0100

    exam03retake01 CdvCheck kész

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -1,4 +1,26 @@
 package exam03retake01;
 
+import java.util.Locale;
+
 public class CdvCheck {
+
+    public boolean check(String taxNumber) {
+        if (taxNumber.length() != 10) {
+            throw new IllegalArgumentException("Wrong taxnumber");
+        }
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            try {
+                String str = taxNumber.substring(i, i + 1);
+                System.out.println(str);
+                int temp = Integer.parseInt(str);
+                sum += temp * (i + 1);
+            } catch (NumberFormatException nfe) {
+                throw new IllegalArgumentException("This character is not digit" + taxNumber.substring(i, i + 1));
+            }
+        }
+        System.out.println(sum % 11);
+        return Integer.parseInt(taxNumber.substring(9)) == sum % 11;
+    }
+
 }
