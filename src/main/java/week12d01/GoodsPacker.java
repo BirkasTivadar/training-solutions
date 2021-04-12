72406456c8acedbf3b2c7e01ad7218c9c0e6704d
BirkasTivadar <birkastivadar@gmail.com>
2021-04-12 17:29:25 +0200

    week12d012, Senior GoodsPacker feladat, Kész, Paraméterellenőrzéssel

--- src/main/java/week12d01/GoodsPacker.java
+++ src/main/java/week12d01/GoodsPacker.java
@@ -1,6 +1,5 @@
 package week12d01;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +7,12 @@ import java.util.List;
 public class GoodsPacker {
 
     public int packGoods(int[][] types, int capacity) {
+        if (types == null) {
+            throw new IllegalArgumentException("Types must not be null");
+        }
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity must be larger than zero.");
+        }
         List<Goods> temp = loadList(types);
         Collections.sort(temp);
         return getMaxValue(temp, capacity);
@@ -27,6 +32,9 @@ public class GoodsPacker {
     private List<Goods> loadList(int[][] types) {
         List<Goods> goods = new ArrayList<>();
         for (int i = 0; i < types.length; i++) {
+            if (types[i] == null) {
+                throw new IllegalArgumentException("Every element of types must not be null.");
+            }
             goods.add(new Goods(types[i][0], types[i][1]));
         }
         return goods;
--- src/test/java/week12d01/GoodsPackerTest.java
+++ src/test/java/week12d01/GoodsPackerTest.java
@@ -7,11 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class GoodsPackerTest {
 
     @Test
+    void testWithNullGoods() {
+        GoodsPacker goodsPacker = new GoodsPacker();
+        int[][] goods = null;
+
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> goodsPacker.packGoods(goods, 1));
+        assertEquals("Types must not be null", iae.getMessage());
+    }
+
+    @Test
+    void testWithNullGood() {
+        GoodsPacker goodsPacker = new GoodsPacker();
+        int[][] goods = {{7, 160}, null, {2, 15}};
+
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> goodsPacker.packGoods(goods, 1));
+        assertEquals("Every element of types must not be null.", iae.getMessage());
+    }
+
+    @Test
+    void testWithMinusCapacity() {
+        GoodsPacker goodsPacker = new GoodsPacker();
+        int[][] goods = {{7, 160}, {3, 90}, {2, 15}};
+
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> goodsPacker.packGoods(goods, -1));
+        assertEquals("Capacity must be larger than zero.", iae.getMessage());
+    }
+
+    @Test
     void testGoodsSort() {
         GoodsPacker goodsPacker = new GoodsPacker();
         int[][] goods = {{7, 160}, {3, 90}, {2, 15}};
-        assertEquals(555, goodsPacker.packGoods(goods, 20));
 
+        assertEquals(555, goodsPacker.packGoods(goods, 20));
+    }
+
+    @Test
+    void testGoodsSortWithRemainCapacity() {
+        GoodsPacker goodsPacker = new GoodsPacker();
+        int[][] goods = {{7, 160}, {3, 90}, {2, 15}};
+
+        assertEquals(540, goodsPacker.packGoods(goods, 19));
     }
 
 }
