fe9405dc53144781e11d2072b1d0a6e9ab2a337d
BirkasTivadar <birkastivadar@gmail.com>
2021-04-16 18:44:22 +0200

    week11d03, Senior CharCounter feladat, Kész, Streammel

--- src/main/java/week11d03/CharCounter.java
+++ src/main/java/week11d03/CharCounter.java
@@ -8,25 +8,14 @@ public class CharCounter {
         if (chars == null || chars.length == 0) {
             throw new IllegalArgumentException("Chars must not be empty");
         }
-        String longestStr = Arrays.stream(chars).max(Comparator.comparing((s -> s.length()))).get();
+        String temp = chars[0];
         int counter = 0;
-        for (char c : longestStr.toCharArray()) {
-            boolean inAll = isInAll(chars, c);
-            if (inAll) {
+        for (char c : temp.toCharArray()) {
+            boolean isTrue = Arrays.stream(chars).allMatch(s -> s.contains(String.valueOf(c)));
+            if(isTrue){
                 counter++;
             }
         }
         return counter;
     }
-
-    private boolean isInAll(String[] chars, char c) {
-        for (String str : chars) {
-            if (!str.contains(String.valueOf(c))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
 }
